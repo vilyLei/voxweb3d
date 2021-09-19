86d1e9488e14d72a6e672b756366aa2d2ae32759
vilyLei <vily313@126.com>
2021-09-20 07:25:32 +0800

    fix: adjust demo code structure.

--- src/demo/DemoCameraPath.ts
+++ src/demo/DemoCameraPath.ts
@@ -6,78 +6,62 @@ import RenderStatusDisplay from "../vox/scene/RenderStatusDisplay";
 import Axis3DEntity from "../vox/entity/Axis3DEntity";
 import Plane3DEntity from "../vox/entity/Plane3DEntity";
 import Box3DEntity from "../vox/entity/Box3DEntity";
-import Sphere3DEntity from "../vox/entity/Sphere3DEntity";
 
-import ImageTextureLoader from "../vox/texture/ImageTextureLoader";
 import MouseEvent from "../vox/event/MouseEvent";
 import KeyboardEvent from "../vox/event/KeyboardEvent";
-import RendererScene from "../vox/scene/RendererScene";
 
-import CameraDragController from "../voxeditor/control/CameraDragController";
-import CameraZoomController from "../voxeditor/control/CameraZoomController";
 import Vector3D from "../vox/math/Vector3D";
 import CameraViewRay from "../vox/view/CameraViewRay";
-import DisplayEntity from "../vox/entity/DisplayEntity";
 import { SpaceCullingMask } from "../vox/space/SpaceCullingMask";
-import { OrthoUIScene } from "../vox/ui/OrthoUIScene";
 import SelectionEvent from "../vox/event/SelectionEvent";
 import SelectionBar from "../orthoui/button/SelectionBar";
+
 import {CameraScene} from "./scene/CameraScene";
+import EngineBase from "../vox/engine/EngineBase";
 
 export class DemoCameraPath {
 
     constructor() { }
 
-    private m_rscene: RendererScene = null;
+    private m_engine: EngineBase = null;
     private m_statusDisp: RenderStatusDisplay = new RenderStatusDisplay();
-    private m_texLoader: ImageTextureLoader = null;
-    private m_runType: number = 0;
-    private m_stageDragCtrl: CameraDragController = new CameraDragController();
-    private m_cameraZoomController: CameraZoomController = new CameraZoomController();
 
     private m_viewRay: CameraViewRay = new CameraViewRay();
-    private m_uiScene: OrthoUIScene = new OrthoUIScene();
     private m_camScene: CameraScene = new CameraScene();
 
     initialize(): void {
 
         console.log("DemoCameraPath::initialize()......");
-        if (this.m_rscene == null) {
+        if (this.m_engine == null) {
 
             //DivLog.SetDebugEnabled( true );
             RendererDevice.SHADERCODE_TRACE_ENABLED = false;
             RendererDevice.VERT_SHADER_PRECISION_GLOBAL_HIGHP_ENABLED = true;
 
+            this.m_engine = new EngineBase();
+            this.m_engine.initialize();
+
             let rparam: RendererParam = new RendererParam();
             rparam.setAttriAntialias(!RendererDevice.IsMobileWeb());
             rparam.setCamPosition(1800.0, 1800.0, 1800.0);
             rparam.setCamProject(45, 20.0, 7000.0);
-            this.m_rscene = new RendererScene();
-            this.m_rscene.initialize(rparam, 3);
-            this.m_rscene.updateCamera();
-
-            this.m_texLoader = new ImageTextureLoader(this.m_rscene.textureBlock);
-
+            
             this.m_statusDisp.initialize();
-            this.m_rscene.addEventListener(MouseEvent.MOUSE_DOWN, this, this.mouseDown);
-            this.m_rscene.addEventListener(KeyboardEvent.KEY_DOWN, this, this.keyDown);
-
-            this.m_viewRay.bindCameraAndStage(this.m_rscene.getCamera(), this.m_rscene.getStage3D());
+            
+            this.m_viewRay.bindCameraAndStage(this.m_engine.rscene.getCamera(), this.m_engine.rscene.getStage3D());
             this.m_viewRay.setPlaneParam(new Vector3D(0.0, 1.0, 0.0), 0.0);
 
-            this.m_uiScene.initialize( this.m_rscene );
+            this.m_engine.rscene.addEventListener(MouseEvent.MOUSE_DOWN, this, this.mouseDown);
+            this.m_engine.rscene.addEventListener(KeyboardEvent.KEY_DOWN, this, this.keyDown);
+            
             this.initBtns();
-            //this.m_rscene.getRenderProxy().setFrontFaceFlipped(true);
+            
             ///*
-            //  let axis:Axis3DEntity = new Axis3DEntity();
-            //  axis.initialize(300.0);
-            //  this.m_rscene.addEntity(axis);
-
             let plane: Plane3DEntity = new Plane3DEntity();
             plane.showDoubleFace();
             plane.uScale = 5.0;
             plane.vScale = 5.0;
-            plane.initializeXOZSquare(1900.0, [this.m_texLoader.getTexByUrl("static/assets/wood_02.jpg")]);
+            plane.initializeXOZSquare(1900.0, [this.m_engine.texLoader.getTexByUrl("static/assets/wood_02.jpg")]);
             plane.setXYZ(0.0, -300.0, 0.0);
             //this.m_rscene.addEntity(plane);
 
@@ -89,21 +73,15 @@ export class DemoCameraPath {
             box.vScale = 4.0;
             //metal_08
             box.showFrontFace();
-            box.initialize(new Vector3D(-size, -size * 0.5, -size), new Vector3D(size, size * 0.7, size), [this.m_texLoader.getTexByUrl("static/assets/brickwall_big.jpg")]);
+            box.initialize(new Vector3D(-size, -size * 0.5, -size), new Vector3D(size, size * 0.7, size), [this.m_engine.texLoader.getTexByUrl("static/assets/brickwall_big.jpg")]);
             box.setXYZ(0.0, 0.0, 0.0);
-            this.m_rscene.addEntity(box);
+            this.m_engine.rscene.addEntity(box);
             //*/
-
-            this.m_rscene.enableMouseEvent(true);
-            this.m_cameraZoomController.bindCamera(this.m_rscene.getCamera());
-            this.m_cameraZoomController.initialize(this.m_rscene.getStage3D());
-            this.m_stageDragCtrl.initialize(this.m_rscene.getStage3D(), this.m_rscene.getCamera());
-            this.m_cameraZoomController.setLookAtCtrlEnabled(false);
 
             this.update();
 
             this.m_camScene = new CameraScene();
-            this.m_camScene.initialize( this.m_rscene );
+            this.m_camScene.initialize( this.m_engine.rscene );
         }
     }
 
@@ -136,33 +114,9 @@ export class DemoCameraPath {
     run(): void {
         
         this.m_statusDisp.update(false);
-
-        this.m_stageDragCtrl.runWithYAxis();
-        this.m_cameraZoomController.run(null, 30.0);
-
-        this.m_camScene.run();
         
-        let pickFlag: boolean = true;
-
-        this.m_uiScene.runBegin(true, true);
-        this.m_uiScene.update(false, true);
-        pickFlag = this.m_uiScene.isRayPickSelected();
-
-        this.m_rscene.runBegin(false);
-        this.m_rscene.update(false, !pickFlag);
-        pickFlag = pickFlag || this.m_rscene.isRayPickSelected();
-
-        /////////////////////////////////////////////////////// ---- mouseTest end.
-
-
-        /////////////////////////////////////////////////////// ---- rendering begin.
-        this.m_rscene.renderBegin();
-        this.m_rscene.run(false);
-        this.m_rscene.runEnd();
-
-        this.m_uiScene.renderBegin();
-        this.m_uiScene.run(false);
-        this.m_uiScene.runEnd();
+        this.m_camScene.run();
+        this.m_engine.run();
         
     }
 
@@ -181,7 +135,7 @@ export class DemoCameraPath {
 
         let selectBar: SelectionBar = new SelectionBar();
         selectBar.uuid = uuid;
-        selectBar.initialize(this.m_uiScene, ns, selectNS, deselectNS, this.m_btnSize);
+        selectBar.initialize(this.m_engine.uiScene, ns, selectNS, deselectNS, this.m_btnSize);
         selectBar.addEventListener(SelectionEvent.SELECT, this, this.selectChange);
         if (flag) {
             selectBar.select(false);
@@ -210,10 +164,10 @@ export class DemoCameraPath {
     private switchSlide(flag: boolean): void {
         
         if (flag) {
-            this.m_stageDragCtrl.enableSlide();
+            this.m_engine.stageDragCtrl.enableSlide();
         }
         else {
-            this.m_stageDragCtrl.enableSwing();
+            this.m_engine.stageDragCtrl.enableSwing();
         }
     }
 }
--- src/vox/engine/EngineBase.ts
+++ src/vox/engine/EngineBase.ts
@@ -16,38 +16,35 @@ export class EngineBase {
     constructor() { }
 
     readonly texLoader: ImageTextureLoader = null;
-    readonly rscene: RendererScene = new RendererScene();
+    readonly rscene: RendererScene = null;//new RendererScene();
     readonly uiScene: OrthoUIScene = new OrthoUIScene();
 
-    private m_stageDragCtrl: CameraDragController = new CameraDragController();
-    private m_cameraZoomController: CameraZoomController = new CameraZoomController();
+    stageDragCtrl: CameraDragController = new CameraDragController();
+    cameraZoomController: CameraZoomController = new CameraZoomController();
 
     viewRay: CameraViewRay = new CameraViewRay();
 
-    initialize(): void {
+    initialize(param: RendererParam = null): void {
 
         console.log("EngineBase::initialize()......");
 
         if (this.rscene == null) {
 
-            //DivLog.SetDebugEnabled( true );
-            //RendererDevice.SHADERCODE_TRACE_ENABLED = false;
-            //RendererDevice.VERT_SHADER_PRECISION_GLOBAL_HIGHP_ENABLED = true;
+            if(param == null) {
+                param = new RendererParam();
+                param.setAttriAntialias(!RendererDevice.IsMobileWeb());
+                param.setCamPosition(1800.0, 1800.0, 1800.0);
+                param.setCamProject(45, 20.0, 7000.0);
+            }
 
-            let rparam: RendererParam = new RendererParam();
-            rparam.setAttriAntialias(!RendererDevice.IsMobileWeb());
-            rparam.setCamPosition(1800.0, 1800.0, 1800.0);
-            rparam.setCamProject(45, 20.0, 7000.0);
-            let scene: RendererScene;
-            scene = new RendererScene();
-            scene.initialize(rparam, 3);
-            scene.updateCamera();
+            let rscene: RendererScene;
+            rscene = new RendererScene();
+            rscene.initialize(param, 3);
+            rscene.updateCamera();
 
             let selfT: any = this;
+            selfT.rscene = rscene;
             selfT.texLoader = new ImageTextureLoader(this.rscene.textureBlock);
-
-            //this.rscene.addEventListener(MouseEvent.MOUSE_DOWN, this, this.mouseDown);
-            //this.rscene.addEventListener(KeyboardEvent.KEY_DOWN, this, this.keyDown);
 
             this.viewRay.bindCameraAndStage(this.rscene.getCamera(), this.rscene.getStage3D());
             this.viewRay.setPlaneParam(new Vector3D(0.0, 1.0, 0.0), 0.0);
@@ -55,18 +52,18 @@ export class EngineBase {
             this.uiScene.initialize( this.rscene );
 
             this.rscene.enableMouseEvent(true);
-            this.m_cameraZoomController.bindCamera(this.rscene.getCamera());
-            this.m_cameraZoomController.initialize(this.rscene.getStage3D());
-            this.m_stageDragCtrl.initialize(this.rscene.getStage3D(), this.rscene.getCamera());
-            this.m_cameraZoomController.setLookAtCtrlEnabled(false);
+            this.cameraZoomController.bindCamera(this.rscene.getCamera());
+            this.cameraZoomController.initialize(this.rscene.getStage3D());
+            this.stageDragCtrl.initialize(this.rscene.getStage3D(), this.rscene.getCamera());
+            this.cameraZoomController.setLookAtCtrlEnabled(false);
 
         }
     }
 
     run(): void {
         
-        this.m_stageDragCtrl.runWithYAxis();
-        this.m_cameraZoomController.run(null, 30.0);
+        this.stageDragCtrl.runWithYAxis();
+        this.cameraZoomController.run(null, 30.0);
 
         let pickFlag: boolean = true;
         
