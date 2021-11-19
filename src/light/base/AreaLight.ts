2ed405d28f78d6461c1767926778e9d62ae49e4e
vilyLei <vily313@126.com>
2021-11-19 18:34:24 +0800

    fix: add some advanced light objects.

--- src/light/base/AreaLight.ts
+++ src/light/base/AreaLight.ts
@@ -8,19 +8,20 @@
 import Vector3D from "../../vox/math/Vector3D";
 import Color4 from "../../vox/material/Color4";
 
-export class DirectionLight {
+export class AreaLight {
 
-    readonly direction: Vector3D = new Vector3D(0.0, -1.0, 0.0, 0.0);
+    readonly position: Vector3D = new Vector3D();
     readonly color: Color4 = new Color4(1.0, 1.0, 1.0, 1.0);
     
     /**
      * 顶点与点光源之间距离的一次方因子, default value is 0.0001
      */
-    attenuationFactor1: number = 0.0;
+    attenuationFactor1: number = 0.0001;
     /**
      * 顶点与点光源之间距离的二次方因子, default value is 0.0005
      */
-    attenuationFactor2: number = 0.0;
+    attenuationFactor2: number = 0.0005;
+
     constructor() { }
 
 }
