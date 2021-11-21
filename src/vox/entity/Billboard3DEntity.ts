eac4a5371b77b768c14bf6083db547a3e2ffd265
vilyLei <vily313@126.com>
2021-11-21 08:12:31 +0800

    fix: optimize Billboard3DEntity class code.

--- src/vox/entity/Billboard3DEntity.ts
+++ src/vox/entity/Billboard3DEntity.ts
@@ -13,149 +13,119 @@ import BillboardMaterial from "../../vox/material/mcase/BillboardMaterial";
 import TextureProxy from "../../vox/texture/TextureProxy";
 import BillboardPlaneMesh from "../../vox/mesh/BillboardPlaneMesh";
 
-export default class Billboard3DEntity extends DisplayEntity
-{
-    private m_brightnessEnabled:boolean = true;
-    private m_alphaEnabled:boolean = false;
-    constructor(transform:ROTransform = null)
-    {
+export default class Billboard3DEntity extends DisplayEntity {
+    private m_brightnessEnabled: boolean = true;
+    private m_alphaEnabled: boolean = false;
+    constructor(transform: ROTransform = null) {
         super(transform);
         this.setRenderState(RendererState.BACK_ADD_BLENDSORT_STATE);
     }
-    flipVerticalUV:boolean = false;
-    private m_bw:number = 0;
-    private m_bh:number = 0;
-    private m_currMaterial:BillboardMaterial = null;
-    private m_billMesh:BillboardPlaneMesh = null;
-    setRGB3f(pr:number,pg:number,pb:number):void
-    {
-        if(this.m_currMaterial != null)
-        {
-            this.m_currMaterial.setRGB3f(pr,pg,pb);
+    flipVerticalUV: boolean = false;
+    private m_bw: number = 0;
+    private m_bh: number = 0;
+    private m_currMaterial: BillboardMaterial = null;
+    private m_billMesh: BillboardPlaneMesh = null;
+    setRGB3f(pr: number, pg: number, pb: number): void {
+        if (this.m_currMaterial != null) {
+            this.m_currMaterial.setRGB3f(pr, pg, pb);
         }
     }
-    setRGBOffset3f(pr:number,pg:number,pb:number):void
-    {
-        if(this.m_currMaterial != null)
-        {
-            this.m_currMaterial.setRGBOffset3f(pr,pg,pb);
+    setRGBOffset3f(pr: number, pg: number, pb: number): void {
+        if (this.m_currMaterial != null) {
+            this.m_currMaterial.setRGBOffset3f(pr, pg, pb);
         }
     }
-    
-    setFadeFactor(pa:number):void
-    {
-        if(this.m_currMaterial != null)
-        {
+
+    setFadeFactor(pa: number): void {
+        if (this.m_currMaterial != null) {
             this.m_currMaterial.setFadeFactor(pa);
         }
     }
-    getFadeFactor():number
-    {
+    getFadeFactor(): number {
         return this.m_currMaterial.getFadeFactor();
     }
-    getRotationZ():number{return this.m_currMaterial.getRotationZ();};
-    setRotationZ(degrees:number):void
-    {
+    getRotationZ(): number { return this.m_currMaterial.getRotationZ(); };
+    setRotationZ(degrees: number): void {
         this.m_currMaterial.setRotationZ(degrees);
     }
-    getScaleX():number{return this.m_currMaterial.getScaleX();}
-    getScaleY():number{return this.m_currMaterial.getScaleY();}
-    setScaleX(p:number):void{this.m_currMaterial.setScaleX(p);}
-    setScaleY(p:number):void{this.m_currMaterial.setScaleY(p);}
-    setScaleXY(sx:number,sy:number):void
-    {
-        this.m_currMaterial.setScaleXY( sx,sy );
+    getScaleX(): number { return this.m_currMaterial.getScaleX(); }
+    getScaleY(): number { return this.m_currMaterial.getScaleY(); }
+    setScaleX(p: number): void { this.m_currMaterial.setScaleX(p); }
+    setScaleY(p: number): void { this.m_currMaterial.setScaleY(p); }
+    setScaleXY(sx: number, sy: number): void {
+        this.m_currMaterial.setScaleXY(sx, sy);
     }
-    createMaterial(texList:TextureProxy[]):void
-    {
-        if(this.getMaterial() == null)
-        {
-            this.m_currMaterial = new BillboardMaterial(this.m_brightnessEnabled,this.m_alphaEnabled);
+    createMaterial(texList: TextureProxy[]): void {
+        if (this.getMaterial() == null) {
+            this.m_currMaterial = new BillboardMaterial(this.m_brightnessEnabled, this.m_alphaEnabled);
             this.m_currMaterial.setTextureList(texList);
             this.setMaterial(this.m_currMaterial);
         }
-        else
-        {
+        else {
             this.m_currMaterial = this.getMaterial() as BillboardMaterial;
             this.m_currMaterial.setTextureList(texList);
         }
     }
-    toTransparentBlend(always:boolean = false):void
-    {
+    toTransparentBlend(always: boolean = false): void {
         this.m_brightnessEnabled = false;
         this.m_alphaEnabled = true;
-        if(always)
-        {
+        if (always) {
             this.setRenderState(RendererState.BACK_TRANSPARENT_STATE);
         }
-        else
-        {
+        else {
             this.setRenderState(RendererState.BACK_TRANSPARENT_ALWAYS_STATE);
         }
     }
-    toBrightnessBlend(always:boolean = false):void
-    {
+    toBrightnessBlend(always: boolean = false): void {
         this.m_brightnessEnabled = true;
         this.m_alphaEnabled = false;
-        if(always)
-        {
+        if (always) {
             this.setRenderState(RendererState.BACK_ADD_BLENDSORT_STATE);
         }
-        else
-        {
+        else {
             this.setRenderState(RendererState.BACK_ADD_BLENDSORT_STATE);
         }
     }
-    initializeSquare(size:number,texList:TextureProxy[]):void
-    {
+    initializeSquare(size: number, texList: TextureProxy[]): void {
         this.m_bw = size;
         this.m_bh = size;
         this.createMaterial(texList);
         this.activeDisplay();
     }
-    initialize(bw:number, bh:number,texList:TextureProxy[]):void
-    {
+    initialize(bw: number, bh: number, texList: TextureProxy[]): void {
         this.m_bw = bw;
         this.m_bh = bh;
         this.createMaterial(texList);
         this.activeDisplay();
     }
-    protected createBounds():void
-    {
+    protected createBounds(): void {
     }
-    protected __activeMesh(material:MaterialBase):void
-    {
-        if(this.getMesh() == null)
-        {
-            let mesh:BillboardPlaneMesh = new BillboardPlaneMesh();
+    protected __activeMesh(material: MaterialBase): void {
+        if (this.getMesh() == null) {
+            let mesh: BillboardPlaneMesh = new BillboardPlaneMesh();
             mesh.vbWholeDataEnabled = this.vbWholeDataEnabled;
             mesh.flipVerticalUV = this.flipVerticalUV;
-            mesh.setBufSortFormat( material.getBufSortFormat() );
+            mesh.setBufSortFormat(material.getBufSortFormat());
             mesh.initialize(this.m_bw, this.m_bh);
             this.setMesh(mesh);
             this.m_billMesh = mesh;
         }
     }
-    
-    setUV(pu:number,pv:number,du:number,dv:number):void
-    {
-        if(this.m_billMesh != null)
-        {
-            this.m_billMesh.setUV(pu,pv,du,dv);
+
+    setUV(pu: number, pv: number, du: number, dv: number): void {
+        if (this.m_billMesh != null) {
+            this.m_billMesh.setUV(pu, pv, du, dv);
         }
     }
-    update():void
-    {
+    update(): void {
         this.m_transfrom.update();
     }
-    destroy():void
-    {
+    destroy(): void {
         this.m_currMaterial = null;
         this.m_billMesh = null;
         super.destroy();
     }
-    toString():string
-    {
-        return "Billboard3DEntity(uid = "+this.getUid()+", rseFlag = "+this.__$rseFlag+")";
+    toString(): string {
+        return "Billboard3DEntity(uid = " + this.getUid() + ", rseFlag = " + this.__$rseFlag + ")";
     }
 }
--- src/vox/material/mcase/BillboardMaterial.ts
+++ src/vox/material/mcase/BillboardMaterial.ts
@@ -15,48 +15,44 @@ import Color4 from "../../../vox/material/Color4";
 import MaterialBase from "../../../vox/material/MaterialBase";
 import BillboardFSBase from "../../../vox/material/mcase/BillboardFSBase";
 
-class BillboardShaderBuffer extends ShaderCodeBuffer
-{
-    billFS:BillboardFSBase = new BillboardFSBase();
-    constructor()
-    {
+class BillboardShaderBuffer extends ShaderCodeBuffer {
+    billFS: BillboardFSBase = new BillboardFSBase();
+    constructor() {
         super();
     }
-    private m_uniqueName:string = "";
-    initialize(texEnabled:boolean):void
-    {
+    private m_uniqueName: string = "";
+    rotationEnabled: boolean = false;
+    initialize(texEnabled: boolean): void {
         this.m_uniqueName = "BillboardShader";
     }
-    getFragShaderCode():string
-    {
+    getFragShaderCode(): string {
         ///*
-        let fragCode0:string =
-`#version 300 es
+        let fragCode0: string =
+            `#version 300 es
 precision mediump float;
 uniform sampler2D u_sampler0;
 in vec4 v_colorMult;
 in vec4 v_colorOffset;
 in vec2 v_texUV;
-layout(location = 0) out vec4 FragColor;
+layout(location = 0) out vec4 FragColor0;
 void main()
 {
-vec4 color = texture(u_sampler0, v_texUV);
-vec3 offsetColor = v_colorOffset.rgb;
-vec4 fv4 = v_colorMult.wwww;
+    vec4 color = texture(u_sampler0, v_texUV);
+    vec3 offsetColor = v_colorOffset.rgb;
+    vec4 fv4 = v_colorMult.wwww;
 `;
-        let fadeCode:string = this.billFS.getBrnAndAlphaCode("fv4");
-        let fragCode2:string =
-`
-FragColor = color;
+        let fadeCode: string = this.billFS.getBrnAndAlphaCode("fv4");
+        let fragCode2: string =
+            `
+    FragColor0 = color;
 }
 `;
         return fragCode0 + fadeCode + fragCode2;
-//*/
+        //*/
     }
-    getVertShaderCode():string
-    {
-        let vtxCode:string = 
-`#version 300 es
+    getVertShaderCode(): string {
+        let vtxCode: string =
+            `#version 300 es
 precision mediump float;
 layout(location = 0) in vec2 a_vs;
 layout(location = 1) in vec2 a_uvs;
@@ -69,34 +65,30 @@ out vec4 v_colorOffset;
 out vec2 v_texUV;
 void main()
 {
-vec4 temp = u_billParam[0];
-float cosv = cos(temp.z);
-float sinv = sin(temp.z);
-vec2 vtx = a_vs.xy * temp.xy;
-vec2 vtx_pos = vec2(vtx.x * cosv - vtx.y * sinv, vtx.x * sinv + vtx.y * cosv);
-vec4 pos = u_viewMat * u_objMat * vec4(0.0,0.0,0.0,1.0);
-pos.xy += vtx_pos.xy;
-gl_Position =  u_projMat * pos;
-v_texUV = a_uvs;
-v_colorMult = u_billParam[1];
-v_colorOffset = u_billParam[2];
+    vec4 temp = u_billParam[0];
+    float cosv = cos(temp.z);
+    float sinv = sin(temp.z);
+    vec2 vtx = a_vs.xy * temp.xy;
+    vec2 vtx_pos = vec2(vtx.x * cosv - vtx.y * sinv, vtx.x * sinv + vtx.y * cosv);
+    vec4 pos = u_viewMat * u_objMat * vec4(0.0,0.0,0.0,1.0);
+    pos.xy += vtx_pos.xy;
+    gl_Position =  u_projMat * pos;
+    v_texUV = a_uvs;
+    v_colorMult = u_billParam[1];
+    v_colorOffset = u_billParam[2];
 }
 `;
         return vtxCode;
     }
-    getUniqueShaderName(): string
-    {
-        return this.m_uniqueName + "_"+this.billFS.getBrnAlphaStatus();
+    getUniqueShaderName(): string {
+        return this.m_uniqueName + "_" + this.billFS.getBrnAlphaStatus();
     }
-    toString():string
-    {
+    toString(): string {
         return "[BillboardShaderBuffer()]";
     }
-    private static s_instance:BillboardShaderBuffer = new BillboardShaderBuffer();
-    static GetInstance():BillboardShaderBuffer
-    {
-        if(BillboardShaderBuffer.s_instance != null)
-        {
+    private static s_instance: BillboardShaderBuffer = new BillboardShaderBuffer();
+    static GetInstance(): BillboardShaderBuffer {
+        if (BillboardShaderBuffer.s_instance != null) {
             return BillboardShaderBuffer.s_instance;
         }
         BillboardShaderBuffer.s_instance = new BillboardShaderBuffer();
@@ -104,90 +96,96 @@ v_colorOffset = u_billParam[2];
     }
 }
 
-export default class BillboardMaterial extends MaterialBase
-{
-    private m_brightnessEnabled:boolean = true;
-    private m_alphaEnabled:boolean = false;
-    constructor(brightnessEnabled:boolean = true,alphaEnabled:boolean = false)
-    {
+export default class BillboardMaterial extends MaterialBase {
+    private m_brightnessEnabled: boolean = true;
+    private m_alphaEnabled: boolean = false;
+    private m_rotationEnabled: boolean = false;
+    private m_rz: number = 0;
+    private m_uniformData: Float32Array = new Float32Array([1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]);
+
+    constructor(brightnessEnabled: boolean = true, alphaEnabled: boolean = false, rotationEnabled: boolean = false) {
+
         super();
         this.m_brightnessEnabled = brightnessEnabled;
         this.m_alphaEnabled = alphaEnabled;
+        this.m_rotationEnabled = rotationEnabled;
     }
-    private m_rz:number = 0;
-    private m_uniformData:Float32Array = new Float32Array([1.0,1.0,0.0,1.0, 1.0,1.0,1.0,1.0, 0.0,0.0,0.0,0.0]);
-    getCodeBuf():ShaderCodeBuffer
-    {
-        let buf:BillboardShaderBuffer = BillboardShaderBuffer.GetInstance();
-        buf.billFS.setBrightnessAndAlpha(this.m_brightnessEnabled,this.m_alphaEnabled);
-        return buf;
+    protected buildBuf(): void {
+        
+        let buf: BillboardShaderBuffer = BillboardShaderBuffer.GetInstance();
+        buf.rotationEnabled = this.m_rotationEnabled;
+        buf.billFS.setBrightnessAndAlpha(this.m_brightnessEnabled, this.m_alphaEnabled);
     }
-    createSelfUniformData():ShaderUniformData
-    {
-        let oum:ShaderUniformData = new ShaderUniformData();
+    getCodeBuf(): ShaderCodeBuffer {
+
+        return BillboardShaderBuffer.GetInstance();
+    }
+    createSelfUniformData(): ShaderUniformData {
+
+        let oum: ShaderUniformData = new ShaderUniformData();
         oum.uniformNameList = ["u_billParam"];
         oum.dataList = [this.m_uniformData];
         return oum;
     }
 
-    
-    setRGBA4f(pr:number,pg:number,pb:number,pa:number):void
-    {
+
+    setRGBA4f(pr: number, pg: number, pb: number, pa: number): void {
+
         this.m_uniformData[4] = pr;
         this.m_uniformData[5] = pg;
         this.m_uniformData[6] = pb;
         this.m_uniformData[7] = pa;
     }
-    setRGB3f(pr:number,pg:number,pb:number)
-    {
+    setRGB3f(pr: number, pg: number, pb: number) {
+
         this.m_uniformData[4] = pr;
         this.m_uniformData[5] = pg;
         this.m_uniformData[6] = pb;
     }
-    setFadeFactor(pa:number):void
-    {
+    setFadeFactor(pa: number): void {
+
         this.m_uniformData[7] = pa;
     }
-    getFadeFactor():number
-    {
+    getFadeFactor(): number {
+
         return this.m_uniformData[7];
     }
 
-    setRGBAOffset4f(pr:number,pg:number,pb:number,pa:number):void
-    {
+    setRGBAOffset4f(pr: number, pg: number, pb: number, pa: number): void {
+
         this.m_uniformData[8] = pr;
         this.m_uniformData[9] = pg;
         this.m_uniformData[10] = pb;
         this.m_uniformData[11] = pa;
     }
-    setRGBOffset3f(pr:number,pg:number,pb:number):void
-    {
+    setRGBOffset3f(pr: number, pg: number, pb: number): void {
+
         this.m_uniformData[8] = pr;
         this.m_uniformData[9] = pg;
         this.m_uniformData[10] = pb;
     }
-    getRotationZ():number{return this.m_rz;};
-    setRotationZ(degrees:number):void
-    {
+    getRotationZ(): number { return this.m_rz; };
+    setRotationZ(degrees: number): void {
+
         this.m_rz = degrees;
         this.m_uniformData[2] = degrees * MathConst.MATH_PI_OVER_180;
     }
-    getScaleX():number{return this.m_uniformData[0];}
-    getScaleY():number{return this.m_uniformData[1];}
-    setScaleX(p:number):void{this.m_uniformData[0] = p;}
-    setScaleY(p:number):void{this.m_uniformData[1] = p;}
-    setScaleXY(sx:number,sy:number):void
-    {
+    getScaleX(): number { return this.m_uniformData[0]; }
+    getScaleY(): number { return this.m_uniformData[1]; }
+    setScaleX(p: number): void { this.m_uniformData[0] = p; }
+    setScaleY(p: number): void { this.m_uniformData[1] = p; }
+    setScaleXY(sx: number, sy: number): void {
+
         this.m_uniformData[0] = sx;
         this.m_uniformData[1] = sy;
     }
-    getUniformData():Float32Array
-    {
+    getUniformData(): Float32Array {
+
         return this.m_uniformData;
     }
 
-    destroy()
-    {
+    destroy() {
+        
         super.destroy();
         this.m_uniformData = null;
     }
