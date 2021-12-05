27b086f0ef5413dce4fbb2a07dc76afd6453775c
vilyLei <vily313@126.com>
2021-12-05 14:56:37 +0800

    fix: adjust pbr material code.

--- src/pbr/material/DefaultPBRShaderCode.d.ts
+++
@@ -1,9 +0,0 @@
-/***************************************************************************/
-/*                                                                         */
-/*  Copyright 2018-2022 by                                                 */
-/*  Vily(vily313@126.com)                                                  */
-/*                                                                         */
-/***************************************************************************/
-import IShaderCodeObject from "../../vox/material/IShaderCodeObject";
-
-export const DefaultPBRShaderCode:IShaderCodeObject;
