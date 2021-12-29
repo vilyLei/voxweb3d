fa432e56e3417fc4a3ce3794243287820c7b9b5b
vilyLei <vily313@126.com>
2021-12-29 09:24:17 +0800

    fix: adjust vertex shader body code.

--- src/pbr/material/glsl/PBR_vertBody.glsl
+++ src/pbr/material/glsl/PBR_vertBody.glsl
@@ -4,8 +4,9 @@
     viewPosition = u_viewMat * worldPosition;
     gl_Position = u_projMat * viewPosition;
 
+    // temporary code plan
     #ifdef VOX_DISPLACEMENT_MAP
-        v_worldPosition = (u_objMat * vec4(a_vs, 1.0)).xyz;
+        v_worldPosition = (u_objMat * vec4(a_vs.xyz, 1.0)).xyz;
     #else
         v_worldPosition = worldPosition.xyz;
     #endif
--- src/vox/material/mcase/glsl/LambertLight_vertBody.glsl
+++ src/vox/material/mcase/glsl/LambertLight_vertBody.glsl
@@ -1,11 +1,15 @@
-initLocalVtx();
+    initLocalVtx();
 
-worldPosition = u_objMat * localPosition;
-viewPosition = u_viewMat * worldPosition;
-gl_Position = u_projMat * viewPosition;
+    worldPosition = u_objMat * localPosition;
+    viewPosition = u_viewMat * worldPosition;
+    gl_Position = u_projMat * viewPosition;
+    // temporary code plan
+    #ifdef VOX_DISPLACEMENT_MAP
+        v_worldPosition = (u_objMat * vec4(a_vs.xyz, 1.0)).xyz;
+    #else
+        v_worldPosition = worldPosition.xyz;
+    #endif
 
-v_worldPosition = worldPosition.xyz;
-
-#ifdef VOX_USE_NORMAL
-    v_worldNormal = normalize(a_nvs.xyz * inverse(mat3(u_objMat)));
-#endif
+    #ifdef VOX_USE_NORMAL
+        v_worldNormal = normalize(a_nvs.xyz * inverse(mat3(u_objMat)));
+    #endif
