95c51c9b0d8da651262fdce13baa42a9176df41e
vilyLei <vily313@126.com>
2022-07-18 06:36:38 +0800

    fix: adjust cors test demo codes.

--- src/cospace/demo/DemoCORS.ts
+++ src/cospace/demo/DemoCORS.ts
@@ -1,4 +1,5 @@
 import { FileIO } from "../../app/slickRoad/io/FileIO";
+import MathConst from "../../vox/math/MathConst";
 import DivLog from "../../vox/utils/DivLog";
 import {FileLoader} from "../modules/loaders/FileLoader";
 
@@ -6,7 +7,6 @@ class NetFileLoader {
 	crossOrigin = 'anonymous';
 	constructor() {
 	}
-
 	setCrossOrigin( crossOrigin: string ): void  {
 		this.crossOrigin = crossOrigin;
 	}
@@ -68,28 +68,39 @@ export class DemoCORS {
 
 	initialize(): void {
 		console.log("DemoCORS::initialize()...");
-
+		
 		// 启用鼠标点击事件
 		document.onmousedown = (evt: any): void => {
 			this.mouseDown(evt);
 		};
+		console.log("ATH.LN2: ", Math.LN2);
 		let url = "http://localhost:9091/static/assets/draco/clothRoll.rawmd";
 		url = "http://localhost:9091/static/assets/bin/u16_8192.bin";
-		this.loadFile( url );
+		url = "http://www.artvily.com:9090/static/assets/bin/u16_8192.bin";
+		for(let i = 0; i < 1000; ++i) {
+			const begin = Math.round(Math.random() * 500);
+			const end = begin + 10 + Math.round(Math.random() * 500)
+			this.loadFile( url, begin * 2, end * 2 );
+		}
 	}
-	private loadFile(url: string): void {
+	private loadFile(url: string, rangeBegin: number, rangeEnd: number): void {
 		let loader = new NetFileLoader();
-
+		let headInfo = "bytes=" + rangeBegin + "-" + rangeEnd;
 		loader.load(url,
 			(buf: ArrayBuffer, url: string): void=>{
-				console.log("DemoCORS::loadFile(), loaded buf:",buf,",url: ",url);
+				// console.log("DemoCORS::loadFile(), loaded buf:",buf,",url: ",url);
 				let u16Buf = new Uint16Array(buf);
-				console.log("u16Buf: ",u16Buf);
+				console.log(headInfo, ", u16Buf: ",u16Buf[0], u16Buf[1], u16Buf[u16Buf.length - 2], u16Buf[u16Buf.length - 1]);
+				let f0 = rangeBegin/2 != u16Buf[0];
+				let f1 = (rangeEnd/2 - 1) != u16Buf[u16Buf.length - 1];
+				if(f0 || f1) {
+					console.error("load range error.");
+				}
 			},
 			null,
 			null,
 			"blob",
-			"bytes=256-4096"
+			headInfo
 
 		)
 	}
