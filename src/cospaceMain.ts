aaac157bdc6e52f96eb281ed370bf98574ba60ed
vilyLei <vily313@126.com>
2022-08-02 07:39:54 +0800

    fix: add draco encode wasm module.

--- src/cospaceMain.ts
+++ src/cospaceMain.ts
@@ -3,7 +3,7 @@
 // import {DemoThreadLoadJS as Demo} from "./cospace/demo/DemoThreadLoadJS";
 // import { DemoCTMLoadAndParser as Demo } from "./cospace/demo/DemoCTMLoadAndParser";
 // import { DemoCTMParser as Demo } from "./cospace/demo/DemoCTMParser";
-// import { DemoDracoParser as Demo } from "./cospace/demo/DemoDracoParser";
+import { DemoDracoParser as Demo } from "./cospace/demo/DemoDracoParser";
 // import { DemoDracoParser2 as Demo } from "./cospace/demo/DemoDracoParser2";
 // import { DemoFBXParser as Demo } from "./cospace/demo/DemoFBXParser";
 // import { DemoGLBParser as Demo } from "./cospace/demo/DemoGLBParser";
@@ -13,7 +13,7 @@
 // import { DemoDependenceGraph as Demo } from "./cospace/demo/DemoDependenceGraph";
 // import { DemoCTMViewer as Demo } from "./cospace/demo/DemoCTMViewer";
 // import { DemoNormalViewer as Demo } from "./cospace/demo/DemoNormalViewer";
-import { RenderingVerifier as Demo } from "./cospace/demo/RenderingVerifier";
+// import { RenderingVerifier as Demo } from "./cospace/demo/RenderingVerifier";
 // import { DemoCoApp as Demo } from "./cospace/demo/DemoCoApp";
 // import { DemoCoEngine as Demo } from "./cospace/demo/DemoCoEngine";
 // import { DemoInputText as Demo } from "./cospace/demo/DemoInputText";
