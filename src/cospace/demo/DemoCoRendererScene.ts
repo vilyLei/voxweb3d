b9eb90528bd172d0a5d73d7f6524dba2d521c578
vilyLei <vily313@126.com>
2022-08-09 20:23:51 +0800

    fix: simpify the CoRendererScene Demo codes.

--- src/cospace/demo/DemoCoRendererScene.ts
+++ src/cospace/demo/DemoCoRendererScene.ts
@@ -1,15 +1,9 @@
-
-import { IRendererInstanceContext } from "../../vox/scene/IRendererInstanceContext";
-import { IRendererInstance } from "../../vox/scene/IRendererInstance";
-
 import { ICoRendererScene } from "../voxengine/scene/ICoRendererScene";
 import { IMouseInteraction } from "../voxengine/ui/IMouseInteraction";
 import { ICoRenderer } from "../voxengine/ICoRenderer";
 import { ICoRScene } from "../voxengine/ICoRScene";
 import { ICoMouseInteraction } from "../voxengine/ui/ICoMouseInteraction";
-
-import Plane3DEntity from "../../vox/entity/Plane3DEntity";
-// import CoRendererScene from "../voxengine/scene/CoRendererScene";
+import { ModuleLoader } from "../modules/base/ModuleLoader";
 
 declare var CoRenderer: ICoRenderer;
 declare var CoRScene: ICoRScene;
@@ -33,12 +27,28 @@ export class DemoCoRendererScene {
 			this.mouseDown(evt);
 		};
 
-		let url: string = "static/cospace/engine/renderer/CoRenderer.umd.min.js";
-		this.loadModule(url);
-		url = "static/cospace/engine/rscene/CoRScene.umd.min.js";
-		this.loadModule(url);
-		url = "static/cospace/engine/mouseInteract/CoMouseInteraction.umd.min.js";
-		this.loadModule(url, "CoMouseInteraction");
+		let url0 = "static/cospace/engine/renderer/CoRenderer.umd.min.js";
+		let url1 = "static/cospace/engine/rscene/CoRScene.umd.min.js";
+		let url2 = "static/cospace/engine/mouseInteract/CoMouseInteraction.umd.min.js";
+
+
+		let mouseInteractML = new ModuleLoader(2);
+		mouseInteractML.setCallback((): void => {
+			this.initInteract();
+		});
+
+		new ModuleLoader(2)
+			.setCallback((): void => {
+				if (this.isEngineEnabled()) {
+					console.log("engine modules loaded ...");
+					this.initRenderer();
+					mouseInteractML.use();
+				}
+			})
+			.loadModule(url0)
+			.loadModule(url1);
+
+		mouseInteractML.loadModule(url2);
 	}
 	isEngineEnabled(): boolean {
 		return typeof CoRenderer !== "undefined" && typeof CoRScene !== "undefined";
@@ -51,29 +61,8 @@ export class DemoCoRendererScene {
 			this.m_interact.setSyncLookAtEnabled( true );
 		}
 	}
-	private loadedModule(module: string): void {
-		if (this.m_rscene == null) {
-			console.log("typeof CoRenderer: ", typeof CoRenderer);
-			console.log("typeof CoRScene: ", typeof CoRScene);
-			if (this.isEngineEnabled()) {
-				console.log("engine 代码块加载完毕");
-				this.initRenderer();
-			}
-		} else {
-			switch(module) {
-				case "CoMouseInteraction":
-					this.initInteract();
-					break;
-				default:
-					break;
-			}
-		}
-	}
 
 	private initRenderer(): void {
-
-		// this.m_rscene = new CoRendererScene();
-		// this.m_rscene.initialize();
 
 		if (this.m_rscene == null) {
 
@@ -94,52 +83,12 @@ export class DemoCoRendererScene {
 
 			let axis = CoRScene.createAxis3DEntity();
 			this.m_rscene.addEntity(axis);
-
-			this.createPlane();
 		}
 	}
 	private mouseDown(evt: any): void {
 
 	}
 
-	private createPlane(): void {
-
-		//     let tex = this.m_rscene.textureBlock.createImageTex2D(64, 64, false);
-
-		//     let plane = new Plane3DEntity();
-		//     plane.initializeXOZ(-400.0, -400.0, 800.0, 800.0, [tex]);
-		//     this.m_rscene.addEntity(plane);
-
-		//     let img: HTMLImageElement = new Image();
-		//     img.onload = (evt: any): void => {
-		//         tex.setDataFromImage(img, 0, 0, 0, false);
-		//     }
-		//     img.src = "static/assets/yanj.jpg";
-	}
-	private loadModule(purl: string, module: string = ""): void {
-
-		let codeLoader: XMLHttpRequest = new XMLHttpRequest();
-		codeLoader.open("GET", purl, true);
-		codeLoader.onerror = function (err) {
-			console.error("load error: ", err);
-		}
-
-		codeLoader.onprogress = e => { };
-		codeLoader.onload = evt => {
-			console.log("engine module js file loaded.");
-			let scriptEle: HTMLScriptElement = document.createElement("script");
-			scriptEle.onerror = evt => {
-				console.error("module script onerror, e: ", evt);
-			};
-			scriptEle.type = "text/javascript";
-			scriptEle.innerHTML = codeLoader.response;
-			document.head.appendChild(scriptEle);
-			this.loadedModule(module);
-		};
-		codeLoader.send(null);
-	}
-
-	private m_time: number = 0.0;
 	run(): void {
 		if (this.m_rscene != null) {
 			if(this.m_interact != null) {
