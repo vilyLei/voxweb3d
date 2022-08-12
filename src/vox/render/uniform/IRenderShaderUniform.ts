cdfec68cfa1b92cfc887a39c1371e3e5be7640e1
vilyLei <vily313@126.com>
2022-08-13 07:03:56 +0800

    fix: optimize shader uniform code file structure.

---
+++ src/vox/render/uniform/IRenderShaderUniform.ts
@@ -0,0 +1,19 @@
+/***************************************************************************/
+/*                                                                         */
+/*  Copyright 2018-2022 by                                                 */
+/*  Vily(vily313@126.com)                                                  */
+/*                                                                         */
+/***************************************************************************/
+
+import IRShaderUniform from "./IRShaderUniform";
+import IRenderShader from "../IRenderShader";
+
+export default interface IRenderShaderUniform extends IRShaderUniform {
+    
+    uns: string;
+    next: IRenderShaderUniform;
+    use(rc: IRenderShader): void;
+    useByLocation(rc: IRenderShader, type: number, location: any, i: number): void;
+    updateData(): void;
+    destroy(): void;
+}
