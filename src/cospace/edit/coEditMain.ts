99b45e099f4d6701ae1381f8d9a6be566747e955
vilyLei <vily313@126.com>
2022-08-18 07:26:37 +0800

    fix: add editor module unit test support in the cospace..

---
+++ src/cospace/edit/coEditMain.ts
@@ -0,0 +1,16 @@
+
+import { DemoCoBase as Demo } from "./demo/DemoCoBase";
+
+let demoIns: Demo = new Demo();
+let ins: any = demoIns;
+function main(): void {
+	console.log("------ demo --- init ------");
+	ins.initialize();
+	function mainLoop(now: any): void {
+		ins.run();
+		window.requestAnimationFrame(mainLoop);
+	}
+	window.requestAnimationFrame(mainLoop);
+	console.log("------ demo --- running ------");
+}
+main();
---
+++ src/cospace/edit/demo/DemoCoBase.ts
@@ -0,0 +1,154 @@
+import { CoDataFormat } from "../../app/CoSpaceAppData";
+
+import { ICoRendererScene } from "../../voxengine/scene/ICoRendererScene";
+import { IMouseInteraction } from "../../voxengine/ui/IMouseInteraction";
+import { ICoRenderer } from "../../voxengine/ICoRenderer";
+import { CoMaterialContextParam, ICoRScene } from "../../voxengine/ICoRScene";
+
+import { ICoMouseInteraction } from "../../voxengine/ui/ICoMouseInteraction";
+import ViewerMaterialCtx from "../../demo/coViewer/ViewerMaterialCtx";
+import { TextPackedLoader } from "../../modules/loaders/TextPackedLoader";
+import { ModuleLoader } from "../../modules/loaders/ModuleLoader";
+import { ViewerCoSApp } from "../../demo/coViewer/ViewerCoSApp";
+import IRenderTexture from "../../../vox/render/texture/IRenderTexture";
+
+//import { DragMoveController } from "../../../../voxeditor/entity/DragMoveController";
+
+declare var CoRenderer: ICoRenderer;
+declare var CoRScene: ICoRScene;
+declare var CoMouseInteraction: ICoMouseInteraction;
+
+
+/**
+ * cospace renderer
+ */
+export class DemoCoBase {
+	private m_rscene: ICoRendererScene = null;
+	private m_interact: IMouseInteraction = null;
+
+	private m_vcoapp: ViewerCoSApp;
+	private m_vmctx: ViewerMaterialCtx;
+
+	constructor() { }
+
+	initialize(): void {
+
+		document.onmousedown = (evt: any): void => {
+			this.mouseDown(evt);
+		};
+		
+		this.initEngineModule();
+
+	}
+
+	private initEngineModule(): void {
+
+		let url = "static/cospace/engine/mouseInteract/CoMouseInteraction.umd.js";
+		let mouseInteractML = new ModuleLoader(2, (): void => {
+			this.initInteract();
+		});
+
+		let url0 = "static/cospace/engine/renderer/CoRenderer.umd.js";
+		let url1 = "static/cospace/engine/rscene/CoRScene.umd.js";
+
+		new ModuleLoader(2, (): void => {
+			if (this.isEngineEnabled()) {
+				console.log("engine modules loaded ...");
+				this.initRenderer();
+
+				this.initScene();
+
+				// this.m_vcoapp = new ViewerCoSApp();
+				// this.m_vcoapp.initialize((): void => {
+				// 	this.loadOBJ();
+				// });
+
+			}
+		}).addLoader(mouseInteractML)
+			.load(url0)
+			.load(url1);
+
+		mouseInteractML.load(url);
+	}
+	private createDefaultEntity(): void {
+
+		let axis = CoRScene.createAxis3DEntity();
+		this.m_rscene.addEntity(axis);
+
+		let texList = [this.createTexByUrl()];
+		let material = CoRScene.createDefaultMaterial();
+		material.setTextureList(texList);
+		let entity = CoRScene.createDisplayEntity();
+		entity.setMaterial(material);
+		entity.copyMeshFrom(this.m_rscene.entityBlock.unitXOZPlane);
+		entity.setScaleXYZ(700.0, 0.0, 700.0);
+		this.m_rscene.addEntity(entity);
+
+	}
+	private initScene(): void {
+
+		this.createDefaultEntity();
+
+	}
+	isEngineEnabled(): boolean {
+		return typeof CoRenderer !== "undefined" && typeof CoRScene !== "undefined";
+	}
+
+    private createTexByUrl(url: string = ""): IRenderTexture {
+
+        let tex = this.m_rscene.textureBlock.createImageTex2D(64, 64, false);
+
+        // this.m_plane = new Plane3DEntity();
+        // this.m_plane.initializeXOZ(-400.0, -400.0, 800.0, 800.0, [tex]);
+        // this.m_rscene.addEntity(this.m_plane);
+
+        let img: HTMLImageElement = new Image();
+        img.onload = (evt: any): void => {
+            tex.setDataFromImage(img, 0, 0, 0, false);
+        }
+        img.src = url != "" ? url: "static/assets/box.jpg";
+		return tex;
+    }
+	private initInteract(): void {
+		if (this.m_rscene != null && this.m_interact == null && typeof CoMouseInteraction !== "undefined") {
+			this.m_interact = CoMouseInteraction.createMouseInteraction();
+			this.m_interact.initialize(this.m_rscene);
+			this.m_interact.setSyncLookAtEnabled(true);
+		}
+	}
+	private initRenderer(): void {
+
+		if (this.m_rscene == null) {
+
+			let RendererDevice = CoRScene.RendererDevice;
+			RendererDevice.SHADERCODE_TRACE_ENABLED = true;
+			RendererDevice.VERT_SHADER_PRECISION_GLOBAL_HIGHP_ENABLED = true;
+			RendererDevice.SetWebBodyColor("#888888");
+
+			let rparam = CoRScene.createRendererSceneParam();
+			rparam.setAttriAntialias(!RendererDevice.IsMobileWeb());
+			rparam.setCamPosition(1000.0, 1000.0, 1000.0);
+			rparam.setCamProject(45, 20.0, 9000.0);
+			this.m_rscene = CoRScene.createRendererScene(rparam, 3);
+			this.m_rscene.setClearUint24Color(0x888888);
+
+		}
+	}
+	private loadOBJ(): void {
+		let baseUrl: string = "static/private/obj/";
+		let url = baseUrl + "base.obj";
+		url = baseUrl + "base4.obj";
+
+	}
+	private mouseDown(evt: any): void { }
+	run(): void {
+		if (this.m_rscene != null) {
+			if (this.m_interact != null) {
+				this.m_interact.run();
+			}
+			this.m_rscene.run();
+		}
+	}
+}
+
+export default DemoCoBase;
