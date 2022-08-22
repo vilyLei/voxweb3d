ce2e605323782462853b72bcb3f1c95924a5f886
vilyLei <vily313@126.com>
2022-08-23 07:50:10 +0800

    fix: add building cross axis entity function in the CoMeshModule.

--- src/cospace/voxmesh/CoMesh.ts
+++ src/cospace/voxmesh/CoMesh.ts
@@ -1,5 +1,6 @@
 import IDataMesh from "../../vox/mesh/IDataMesh";
 import IRawMesh from "../../vox/mesh/IRawMesh";
+import ITransformEntity from "../../vox/entity/ITransformEntity";
 
 import { ICoRScene } from "../voxengine/ICoRScene";
 declare var CoRScene: ICoRScene;
@@ -12,8 +13,14 @@ function createDataMesh(): IDataMesh {
 function createRawMesh(): IRawMesh {
 	return CoRScene.createRawMesh();
 }
+function createCrossAxis3DEntity(size: number = 100): ITransformEntity {
+	let min = -0.5 * size;
+	let max = 0.5 * size;
+	return CoRScene.createFreeAxis3DEntity(CoRScene.createVec3(min,min,min), CoRScene.createVec3(max,max,max));
+}
 
 export {
 	createDataMesh,
-	createRawMesh
+	createRawMesh,
+	createCrossAxis3DEntity,
 };
--- src/cospace/voxmesh/ICoMesh.ts
+++ src/cospace/voxmesh/ICoMesh.ts
@@ -1,9 +1,14 @@
 import IDataMesh from "../../vox/mesh/IDataMesh";
 import IRawMesh from "../../vox/mesh/IRawMesh";
+import ITransformEntity from "../../vox/entity/ITransformEntity";
 
 interface ICoMesh {
 
 	createDataMesh(): IDataMesh;
 	createRawMesh(): IRawMesh;
+	/**
+	 * @param size the default value is 100
+	 */
+	createCrossAxis3DEntity(size: number): ITransformEntity;
 }
 export { ICoMesh };
