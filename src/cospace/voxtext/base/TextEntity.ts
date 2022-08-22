8697e7dd0792516f555c6eb30a1cd0952318b7ed
vilyLei <vily313@126.com>
2022-08-22 22:03:11 +0800

    mfix: add vox text module, and some text entity.

---
+++ src/cospace/voxtext/base/TextEntity.ts
@@ -0,0 +1,137 @@
+/***************************************************************************/
+/*                                                                         */
+/*  Copyright 2018-2022 by                                                 */
+/*  Vily(vily313@126.com)                                                  */
+/*                                                                         */
+/***************************************************************************/
+
+import RendererState from "../../../vox/render/RendererState";
+import DisplayEntity from "../../../vox/entity/DisplayEntity";
+import IRenderMaterial from '../../../vox/render/IRenderMaterial';
+import BillboardAlphaTexMaterial from "../../../vox/material/mcase/BillboardAlphaTexMaterial";
+import IRenderTexture from "../../../vox/render/texture/IRenderTexture";
+import TextMeshBuilder from "./TextMeshBuilder";
+import H5Text from "./H5Text";
+
+export default class TextEntity extends DisplayEntity {
+
+    private m_dynamicEnbaled: boolean = true;
+    private m_material: BillboardAlphaTexMaterial = null;
+    private m_width: number = 0;
+    private m_height: number = 0;
+
+    private m_text: string = "";
+    private m_alignFactorX: number = 0.5;
+    private m_alignFactorY: number = 0.5;
+    private m_h5Text: H5Text;
+
+    flipVerticalUV = false;
+    constructor(dynamicEnbaled: boolean = true) {
+        super();
+        this.m_dynamicEnbaled = dynamicEnbaled;
+    }
+    getWidth(): number {
+        return this.m_width;
+    }
+    getHeight(): number {
+        return this.m_height;
+    }
+    setAlignFactor(alignFactorX: number, alignFactorY: number): void {
+        this.m_alignFactorX = alignFactorX;
+        this.m_alignFactorY = alignFactorY;
+    }
+    alignLeftTop(): void {
+        this.setAlignFactor(0.0, 0.0);
+    }
+    alignCenter(): void {
+        this.setAlignFactor(0.5, 0.5);
+    }
+    alignLeftCenter(): void {
+        this.setAlignFactor(0.0, 0.5);
+    }
+    setText(charsStr: string): void {
+
+    }
+    setRGBA4f(pr: number, pg: number, pb: number, pa: number): void {
+        if (this.m_material != null) {
+            this.m_material.setRGBA4f(pr, pg, pb, pa);
+        }
+    }
+    setRGB3f(pr: number, pg: number, pb: number): void {
+        if (this.m_material != null) {
+            this.m_material.setRGB3f(pr, pg, pb);
+        }
+    }
+    setAlpha(pa: number): void {
+        this.m_material.setAlpha(pa);
+    }
+    getAlpha(): number {
+        return this.m_material.getAlpha();
+    }
+    setBrightness(brighness: number): void {
+        this.m_material.setBrightness(brighness);
+    }
+    getBrightness(): number {
+        return this.m_material.getBrightness();
+    }
+    getRotationZ(): number { return this.m_material.getRotationZ(); };
+    setRotationZ(degrees: number): void {
+        this.m_material.setRotationZ(degrees);
+    }
+    getScaleX(): number { return this.m_material.getScaleX(); }
+    getScaleY(): number { return this.m_material.getScaleY(); }
+    setScaleX(p: number): void { this.m_material.setScaleX(p); }
+    setScaleY(p: number): void { this.m_material.setScaleY(p); }
+    setScaleXY(sx: number, sy: number): void {
+        this.m_material.setScaleXY(sx, sy);
+    }
+    createMaterial(texList: IRenderTexture[]): void {
+        if (this.getMaterial() == null) {
+            this.m_material = new BillboardAlphaTexMaterial();
+            this.m_material.setTextureList(texList);
+            this.setMaterial(this.m_material);
+        }
+        else {
+            this.m_material.setTextureList(texList);
+        }
+    }
+    initialize(text: string, h5Text: H5Text, texList: IRenderTexture[] = null): void {
+        this.m_text = text;
+		this.m_h5Text = h5Text;
+        if (texList == null) {
+            this.createMaterial([h5Text.getTextureAt(0)]);
+        }
+        else {
+            this.createMaterial(texList);
+        }
+        this.activeDisplay();
+        this.setRenderState(RendererState.BACK_TRANSPARENT_STATE);
+    }
+    protected createBounds(): void {
+    }
+    protected __activeMesh(material: IRenderMaterial): void {
+        if (this.getMesh() == null) {
+			let builder = new TextMeshBuilder();
+            builder.vbWholeDataEnabled = this.vbWholeDataEnabled;
+            builder.alignFactorX = this.m_alignFactorX;
+            builder.alignFactorY = this.m_alignFactorY;
+            builder.initialize(this.m_h5Text);
+			this.m_width = builder.getWidth();
+			this.m_height = builder.getHeight();
+            let mesh = builder.create(this.m_text);
+            this.setMesh(mesh);
+        }
+    }
+    update(): void {
+        if(this.m_transfrom != null) {
+            if (this.m_dynamicEnbaled && this.m_mesh.isGeomDynamic()) {
+                this.setIvsParam(0, this.m_mesh.vtCount);
+            }
+            this.m_transfrom.update();
+        }
+    }
+    destroy(): void {
+        super.destroy();
+        this.m_h5Text = null;
+    }
+}
---
+++ src/cospace/voxtext/base/TextMeshBuilder.ts
@@ -0,0 +1,205 @@
+/***************************************************************************/
+/*                                                                         */
+/*  Copyright 2018-2022 by                                                 */
+/*  Vily(vily313@126.com)                                                  */
+/*                                                                         */
+/***************************************************************************/
+
+import IAABB from "../../../vox/geom/IAABB";
+import IRawMesh from "../../../vox/mesh/IRawMesh";
+import H5Text from "./H5Text";
+
+import RawMesh from "../../../vox/mesh/RawMesh";
+
+// import { ICoRScene } from "../../voxengine/ICoRScene";
+// declare var CoRScene: ICoRScene;
+
+export default class TextMeshBuilder {
+	constructor() {}
+	private m_ivs: Uint16Array = null;
+	private m_vs: Float32Array = null;
+	private m_uvs: Float32Array = null;
+	private m_h5Text: H5Text = null;
+
+	private m_width: number = 0;
+	private m_height: number = 0;
+	private m_charsTotal: number = 0;
+	private m_text: string = "";
+
+	flipVerticalUV: boolean = false;
+	vtxUVEnabled: boolean = true;
+	vertColorEnabled: boolean = false;
+	alignFactorX: number = 0.5;
+	alignFactorY: number = 0.5;
+	vbWholeDataEnabled: boolean = true;
+	getWidth(): number {
+		return this.m_width;
+	}
+	getHeight(): number {
+		return this.m_height;
+	}
+	private static s_initIvs: Uint8Array = new Uint8Array([0, 1, 2, 0, 2, 3]);
+	private static s_currIvs: Uint8Array = new Uint8Array([0, 1, 2, 0, 2, 3]);
+	private static s_sizeArr: number[] = [0, 0];
+
+	private createMesh(text: string, mesh: IRawMesh): IRawMesh {
+		this.m_text = text;
+		let charsTot = text.length;
+		this.m_charsTotal = charsTot;
+
+		if (charsTot > 0) {
+			let tmb = TextMeshBuilder;
+
+			let ivs: Uint8Array = tmb.s_currIvs;
+			ivs.set(tmb.s_initIvs, 0);
+			if (this.m_ivs == null) {
+				this.m_ivs = new Uint16Array(charsTot * 6);
+				for (let i: number = 0; i < charsTot; ++i) {
+					this.m_ivs.set(ivs, i * 6);
+					ivs[0] += 4;
+					ivs[1] += 4;
+					ivs[2] += 4;
+					ivs[3] += 4;
+					ivs[4] += 4;
+					ivs[5] += 4;
+				}
+			}
+
+			let expand = false;
+			let h5Text = this.m_h5Text;
+
+			h5Text.createCharsTexFromStr(text);
+			this.m_width = -1;
+			this.m_height = 0;
+			let sizeArr: number[] = tmb.s_sizeArr;
+			let i: number = 0;
+			let maxX: number = 0;
+			let maxY: number = 0;
+			let minX: number = 0;
+			let minY: number = 0;
+			expand = this.m_vs != null && charsTot * 8 > this.m_vs.length;
+			let charTable = h5Text.getCharTable();
+			if (expand) {
+				let ivs = tmb.s_currIvs;
+				ivs.set(tmb.s_initIvs, 0);
+				this.m_ivs = new Uint16Array(charsTot * 6);
+				this.m_uvs = new Float32Array(charsTot * 8);
+				this.m_vs = new Float32Array(charsTot * 8);
+				for (; i < charsTot; ++i) {
+					this.m_ivs.set(ivs, i * 6);
+					ivs[0] += 4;
+					ivs[1] += 4;
+					ivs[2] += 4;
+					ivs[3] += 4;
+					ivs[4] += 4;
+					ivs[5] += 4;
+					charTable.getUV8AndSizeFromChar(text.charAt(i), this.m_uvs, sizeArr, i * 8);
+					if (this.m_height < sizeArr[1]) {
+						this.m_height = sizeArr[1];
+					}
+					this.m_vs[i * 8] = sizeArr[0];
+					this.m_width += sizeArr[0];
+				}
+			} else {
+				if (this.m_vs == null) {
+					this.m_uvs = new Float32Array(charsTot * 8);
+					this.m_vs = new Float32Array(charsTot * 8);
+				}
+
+				for (; i < charsTot; ++i) {
+					charTable.getUV8AndSizeFromChar(text.charAt(i), this.m_uvs, sizeArr, i * 8);
+					if (this.m_height < sizeArr[1]) {
+						this.m_height = sizeArr[1];
+					}
+					this.m_vs[i * 8] = sizeArr[0];
+					this.m_width += sizeArr[0];
+				}
+			}
+			let dis = 1.0;
+			this.m_width += charsTot * dis;
+			minX = -this.alignFactorX * this.m_width;
+			minY = -this.alignFactorY * this.m_height;
+			maxY = minY + this.m_height;
+			let j: number = 0;
+			for (i = 0; i < charsTot; ++i) {
+				maxX = minX + this.m_vs[j];
+				this.m_vs[j] = minX;
+				this.m_vs[j + 1] = minY;
+				this.m_vs[j + 2] = maxX;
+				this.m_vs[j + 3] = minY;
+				this.m_vs[j + 4] = maxX;
+				this.m_vs[j + 5] = maxY;
+				this.m_vs[j + 6] = minX;
+				this.m_vs[j + 7] = maxY;
+				minX = maxX + dis;
+				j += 8;
+			}
+
+			// let mesh = CoRScene.createRawMesh();
+			mesh = mesh == null ? new RawMesh() : mesh;
+			mesh.reset();
+			mesh.setIVS(this.m_ivs);
+			mesh.addFloat32Data(this.m_vs, 2);
+			mesh.addFloat32Data(this.m_uvs, 2);
+			mesh.vbWholeDataEnabled = this.vbWholeDataEnabled;
+			mesh.initialize();
+			mesh.vtCount = charsTot * 6;
+			mesh.vtxTotal = charsTot * 4;
+			mesh.trisNumber = charsTot * 2;
+			return mesh;
+		}
+		return null;
+	}
+
+	initialize(h5Text: H5Text): void {
+		this.m_h5Text = h5Text;
+	}
+	create(text: string, mesh: IRawMesh = null): IRawMesh {
+		return this.createMesh(text, mesh);
+	}
+	/*
+    initialize(text: string, h5Text: H5Text): void {
+        //console.log("RectPlaneMesh::initialize()...");
+		this.m_h5Text = h5Text;
+        let charsTot = text.length;
+        let ivs = TextMeshBuilder.s_currIvs;
+        ivs.set(TextMeshBuilder.s_initIvs, 0);
+        if (this.m_ivs == null) {
+            this.m_ivs = new Uint16Array(charsTot * 6);
+        }
+        for (let i: number = 0; i < charsTot; ++i) {
+            this.m_ivs.set(ivs, i * 6);
+            ivs[0] += 4;
+            ivs[1] += 4;
+            ivs[2] += 4;
+            ivs[3] += 4;
+            ivs[4] += 4;
+            ivs[5] += 4;
+        }
+        this.createMesh(text);
+
+        // ROVertexBuffer.vbWholeDataEnabled = this.vbWholeDataEnabled;
+        // this.m_vbuf = ROVertexBuffer.CreateBySaveData(this.getBufDataUsage());
+        // this.m_vbuf.setUintIVSData(this.m_ivs);
+        // //this.drawMode = RenderDrawMode.ELEMENTS_TRIANGLE_STRIP;
+        // this.buildEnd();
+    }
+	//*/
+	// updateCharStr(text: string): void {
+	//     // if (this.m_vbuf != null && this.m_text != text) {
+	//     //     this.createMesh(text);
+	//     //     this.buildEnd();
+	//     //     ROVertexBuffer.UpdateBufData(this.m_vbuf);
+	//     // }
+	// }
+	destroy(): void {
+		// if (this.isResFree()) {
+		//     this.bounds = null;
+
+		//     this.m_vs = null;
+		//     this.m_uvs = null;
+		//     super.__$destroy();
+		// }
+		this.m_h5Text = null;
+	}
+}
