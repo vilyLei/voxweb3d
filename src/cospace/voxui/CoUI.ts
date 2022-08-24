8491d9086e800fa78a5e87f42b7598c6481104a8
vilyLei <vily313@126.com>
2022-08-25 07:52:07 +0800

    fix: improve the CoUI module.

--- src/cospace/voxui/CoUI.ts
+++ src/cospace/voxui/CoUI.ts
@@ -1,9 +1,26 @@
+import { IClipLable } from "./entity/IClipLable";
+import { ClipLable } from "./entity/ClipLable";
 
-import { ICoRScene } from "../voxengine/ICoRScene";
-declare var CoRScene: ICoRScene;
-function test(): void {
-	
+import { IButton } from "./entity/IButton";
+import { Button } from "./entity/Button";
+
+import { ICoUIScene } from "./scene/ICoUIScene";
+import { CoUIScene } from "./scene/CoUIScene";
+
+// import { ICoRScene } from "../voxengine/ICoRScene";
+// declare var CoRScene: ICoRScene;
+
+function createClipLabel(): IClipLable {
+	return new ClipLable();
+}
+function createButton(): IButton {
+	return new Button();
+}
+function createUIScene(): ICoUIScene {
+	return new CoUIScene();
 }
 export {
-	test
+	createClipLabel,
+	createButton,
+	createUIScene
 };
--- src/cospace/voxui/ICoUI.ts
+++ src/cospace/voxui/ICoUI.ts
@@ -1,6 +1,12 @@
+import { IClipLable } from "./entity/IClipLable";
+
+import { IButton } from "./entity/IButton";
+
+import { ICoUIScene } from "./scene/ICoUIScene";
 
 interface ICoUI {
-
-	test(): void;
+	createClipLabel(): IClipLable;
+	createButton(): IButton;
+	createUIScene(): ICoUIScene;
 }
 export { ICoUI };
