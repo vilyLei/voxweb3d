38aaee465326f21942d5bfc8c0f9892fc0ee9345
vilyLei <vily313@126.com>
2022-08-27 09:27:49 +0800

    fix: add the code/funtion modules management strategy.

---
+++ src/cospace/modules/manage/CoSpaceModule.ts
@@ -0,0 +1,8 @@
+/**
+ * manage cospace code/function modules  in main thread with their dependencies.
+ * 不同的实现可以建立不同应用过程的模块依赖关系动态组织机制
+ */
+export default class CoSpaceModule {
+	constructor() {
+	}
+}
