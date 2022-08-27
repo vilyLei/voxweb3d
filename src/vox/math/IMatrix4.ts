b5f35fa86eabb3744fdca842297c6273b385d782
vilyLei <vily313@126.com>
2022-08-27 09:44:46 +0800

    fix: optimize matrix4 interface.

--- src/vox/math/IMatrix4.ts
+++ src/vox/math/IMatrix4.ts
@@ -5,7 +5,7 @@
 /*                                                                         */
 /***************************************************************************/
 
-import Vector3D from "../../vox/math/Vector3D";
+import IVector3D from "../../vox/math/IVector3D";
 import Float32Data from "../../vox/base/Float32Data";
 
 interface IMatrix4 extends Float32Data {
@@ -23,11 +23,11 @@ interface IMatrix4 extends Float32Data {
 	/**
 	 *
 	 * @param radian rotation angle radian
-	 * @param axis rotation axis, it is a normalized Vector3D instance
+	 * @param axis rotation axis, it is a normalized IVector3D instance
 	 * @param pivotPoint the default value is null
 	 */
-	appendRotationPivot(radian: number, axis: Vector3D, pivotPoint?: Vector3D): void;
-	appendRotation(radian: number, axis: Vector3D): void;
+	appendRotationPivot(radian: number, axis: IVector3D, pivotPoint?: IVector3D): void;
+	appendRotation(radian: number, axis: IVector3D): void;
 	appendRotationX(radian: number): void;
 	appendRotationY(radian: number): void;
 	appendRotationZ(radian: number): void;
@@ -38,14 +38,14 @@ interface IMatrix4 extends Float32Data {
 	setRotationEulerAngle(radianX: number, radianY: number, radianZ: number): void;
 	setRotationEulerAngle2(cosX: number, sinX: number, cosY: number, sinY: number, cosZ: number, sinZ: number): void;
 	setTranslationXYZ(px: number, py: number, pz: number): void;
-	setTranslation(v3: Vector3D): void;
+	setTranslation(v3: IVector3D): void;
 	appendScaleXYZ(xScale: number, yScale: number, zScale: number): void;
 
 	appendScaleXY(xScale: number, yScale: number): void;
 	appendTranslationXYZ(px: number, py: number, pz: number): void;
-	appendTranslation(v3: Vector3D): void;
-	copyColumnFrom(column_index: number, v3: Vector3D): void;
-	copyColumnTo(column_index: number, v3: Vector3D): void;
+	appendTranslation(v3: IVector3D): void;
+	copyColumnFrom(column_index: number, v3: IVector3D): void;
+	copyColumnTo(column_index: number, v3: IVector3D): void;
 	/**
 	 * @param fs32Arr src data
 	 * @param index the default value is 0
@@ -86,15 +86,15 @@ interface IMatrix4 extends Float32Data {
 	 * @param transpose  the default value is false
 	 */
 	copyRawDataTo(float_rawDataArr: Float32Array, rawDataLength?: number, index?: number, transpose?: boolean): void;
-	copyRowFrom(row_index: number, v3: Vector3D): void;
-	copyRowTo(row_index: number, v3: Vector3D): void;
-	decompose(orientationStyle: number): Vector3D[];
+	copyRowFrom(row_index: number, v3: IVector3D): void;
+	copyRowTo(row_index: number, v3: IVector3D): void;
+	decompose(orientationStyle: number): IVector3D[];
 	invert(): boolean;
-	pointAt(pos: Vector3D, at: Vector3D, up: Vector3D): void;
+	pointAt(pos: IVector3D, at: IVector3D, up: IVector3D): void;
 	prepend(rhs: IMatrix4): void;
 	prepend3x3(rhs: IMatrix4): void;
-	prependRotationPivot(radian: number, axis: Vector3D, pivotPoint: Vector3D): void;
-	prependRotation(radian: number, axis: Vector3D): void;
+	prependRotationPivot(radian: number, axis: IVector3D, pivotPoint: IVector3D): void;
+	prependRotation(radian: number, axis: IVector3D): void;
 	prependRotationX(radian: number): void;
 	prependRotationY(radian: number): void;
 	prependRotationZ(radian: number): void;
@@ -103,18 +103,18 @@ interface IMatrix4 extends Float32Data {
 	prependScale(xScale: number, yScale: number, zScale: number): void;
 	prependScaleXY(xScale: number, yScale: number): void;
 	prependTranslationXYZ(px: number, py: number, pz: number): void;
-	prependTranslation(v3: Vector3D): void;
-	recompose(components: Vector3D[], orientationStyle: number): boolean;
-	setThreeAxes(x_axis: Vector3D, y_axis: Vector3D, z_axis: Vector3D,): void;
-	deltaTransformVector(v3: Vector3D): Vector3D;
+	prependTranslation(v3: IVector3D): void;
+	recompose(components: IVector3D[], orientationStyle: number): boolean;
+	setThreeAxes(x_axis: IVector3D, y_axis: IVector3D, z_axis: IVector3D,): void;
+	deltaTransformVector(v3: IVector3D): IVector3D;
 
-	deltaTransformVectorSelf(v3: Vector3D): void;
-	deltaTransformOutVector(v3: Vector3D, out_v3: Vector3D): void;
-	transformVector(v3: Vector3D): Vector3D;
-	transformOutVector(v3: Vector3D, out_v3: Vector3D): void;
-	transformOutVector3(v3: Vector3D, out_v3: Vector3D): void;
-	transformVector3Self(v3: Vector3D): void;
-	transformVectorSelf(v3: Vector3D): void;
+	deltaTransformVectorSelf(v3: IVector3D): void;
+	deltaTransformOutVector(v3: IVector3D, out_v3: IVector3D): void;
+	transformVector(v3: IVector3D): IVector3D;
+	transformOutVector(v3: IVector3D, out_v3: IVector3D): void;
+	transformOutVector3(v3: IVector3D, out_v3: IVector3D): void;
+	transformVector3Self(v3: IVector3D): void;
+	transformVectorSelf(v3: IVector3D): void;
 	transformVectors(float_vinArr: Float32Array | number[], vinLength: number, float_voutArr: Float32Array): void;
 	transformVectorsSelf(float_vinArr: Float32Array | number[], vinLength: number): void;
 	transformVectorsRangeSelf(float_vinArr: Float32Array | number[], begin: number, end: number): void;
@@ -126,7 +126,7 @@ interface IMatrix4 extends Float32Data {
 
 	copyTranslation( m: IMatrix4 ): IMatrix4;
 
-	transformPerspV4Self(v4: Vector3D): void;
+	transformPerspV4Self(v4: IVector3D): void;
 
 	premultiply(m: IMatrix4): IMatrix4;
 	multiply(m: IMatrix4): IMatrix4;
