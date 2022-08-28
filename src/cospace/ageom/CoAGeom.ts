80bfea3bb8ccd2734971aa5a89e2404e8df89ca8
vilyLei <vily313@126.com>
2022-08-28 21:36:01 +0800

    fix: add some codes.

---
+++ src/cospace/ageom/CoAGeom.ts
@@ -0,0 +1,44 @@
+import Line from "./base/Line";
+import ILine from "./base/ILine";
+import IRayLine from "./base/IRayLine";
+import RayLine from "./base/RayLine";
+import ISegmentLine from "./base/ISegmentLine";
+import SegmentLine from "./base/SegmentLine";
+import IPlane from "./base/IPlane";
+import Plane from "./base/Plane";
+import PlaneUtils from "./base/PlaneUtils";
+import ISphere from "./base/ISphere";
+import Sphere from "./base/Sphere";
+
+
+function createLine(): ILine {
+	return new Line();
+}
+function createRayLine(): IRayLine {
+	return new RayLine();
+}
+function createSegmentLine(): ISegmentLine {
+	return new SegmentLine();
+}
+function createPlane(): IPlane {
+	return new Plane();
+}
+function createSphere(): ISphere {
+	return new Sphere();
+}
+
+export {
+
+	Line,
+	RayLine,
+	SegmentLine,
+	Plane,
+	PlaneUtils,
+	Sphere,
+
+	createLine,
+	createRayLine,
+	createSegmentLine,
+	createPlane,
+	createSphere
+}
