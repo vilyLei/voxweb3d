ce371ccb8d80b217e6b87d3ab9ec12c7103f9d96
vilyLei <vily313@126.com>
2022-09-23 22:35:23 +0800

    fix: improve the UserEditCtr class functions.

--- src/cospace/edit/base/UserEditCtr.ts
+++ src/cospace/edit/base/UserEditCtr.ts
@@ -5,7 +5,14 @@ class UserEditCtr {
     runningVisible: boolean = true;
     uuid: string = "editCtrl";
     moveSelfEnabled: boolean = true;
-    
+
+    protected editBegin(): void {
+        console.log("UserEditCtr::editBegin()");
+    }
+    protected editEnd(): void {
+        console.log("UserEditCtr::editEnd()");
+    }
+
     enable(): void {
         this.m_enabled = true;
     }
