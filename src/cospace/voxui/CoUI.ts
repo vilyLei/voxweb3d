675dd65f607d844e9ec6d566656273ac9db0c2e3
vilyLei <vily313@126.com>
2022-09-29 22:41:54 +0800

    fix: improve exporting functions in the CoUI module.

--- src/cospace/voxui/CoUI.ts
+++ src/cospace/voxui/CoUI.ts
@@ -3,14 +3,20 @@ import { ClipLabel } from "./entity/ClipLabel";
 import { IClipColorLabel } from "./entity/IClipColorLabel";
 import { ClipColorLabel } from "./entity/ClipColorLabel";
 import { ColorClipLabel } from "./entity/ColorClipLabel";
+import { ColorLabel } from "./entity/ColorLabel";
 
 import { IButton } from "./entity/IButton";
 import { Button } from "./entity/Button";
-
 import { ICoUIScene } from "./scene/ICoUIScene";
 import { CoUIScene } from "./scene/CoUIScene";
 import { RectTextTip } from "./entity/RectTextTip";
+import { TipInfo } from "./base/TipInfo";
+import { UILayout } from "./layout/UILayout";
 
+
+function createColorLabel(): ColorLabel {
+	return new ColorLabel();
+}
 function createRectTextTip(): RectTextTip {
 	return new RectTextTip();
 }
@@ -30,7 +36,18 @@ function createButton(): IButton {
 function createUIScene(): ICoUIScene {
 	return new CoUIScene();
 }
+function createTipInfo(): TipInfo {
+	return new TipInfo();
+}
+function createUILayout(): UILayout {
+	return new UILayout();
+}
+
 export {
+	
+	createColorLabel,
+	createUILayout,
+	createTipInfo,
 	createRectTextTip,
 	createClipLabel,
 	createClipColorLabel,
--- src/cospace/voxui/ICoUI.ts
+++ src/cospace/voxui/ICoUI.ts
@@ -1,12 +1,18 @@
 import { IClipLabel } from "./entity/IClipLabel";
 import { IClipColorLabel } from "./entity/IClipColorLabel";
 import { IColorClipLabel } from "./entity/IColorClipLabel";
+import { IColorLabel } from "./entity/IColorLabel";
 import { IButton } from "./entity/IButton";
 import { ICoUIScene } from "./scene/ICoUIScene";
 import { IRectTextTip } from "./entity/IRectTextTip";
+import { ITipInfo } from "./base/ITipInfo";
+import { IUILayout } from "./layout/IUILayout";
 
 interface ICoUI {
-	
+	createColorLabel(): IColorLabel;
+	createUILayout(): IUILayout;
+	createTipInfo(): ITipInfo;
+	createRectTextTip(): IRectTextTip;
 	createRectTextTip(): IRectTextTip;
 	createClipLabel(): IClipLabel;
 	createClipColorLabel(): IClipColorLabel;
