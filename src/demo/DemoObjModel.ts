98a1dcf73ab7b7cfbccc8302165f4f9c95b89642
vilyLei <vily313@126.com>
2022-10-12 20:48:53 +0800

    fix: adjust some codes.

--- src/demo/DemoObjModel.ts
+++ src/demo/DemoObjModel.ts
@@ -2,6 +2,7 @@
 import RendererDevice from "../vox/render/RendererDevice";
 import { RenderBlendMode, CullFaceMode, DepthTestMode } from "../vox/render/RenderConst";
 import RendererState from "../vox/render/RendererState";
+import MouseEvent from "../vox/event/MouseEvent";
 import RendererParam from "../vox/scene/RendererParam";
 import RendererScene from "../vox/scene/RendererScene";
 import RenderStatusDisplay from "../vox/scene/RenderStatusDisplay";
@@ -14,6 +15,7 @@ import ObjData3DEntity from "../vox/entity/ObjData3DEntity";
 import { UserInteraction } from "../vox/engine/UserInteraction";
 import Axis3DEntity from "../vox/entity/Axis3DEntity";
 import Vector3D from "../vox/math/Vector3D";
+import DisplayEntity from "../vox/entity/DisplayEntity";
 
 export class DemoObjModel {
     constructor() { }
@@ -22,6 +24,8 @@ export class DemoObjModel {
     private m_texLoader: ImageTextureLoader;
     private m_statusDisp: RenderStatusDisplay = new RenderStatusDisplay();
     private m_interation: UserInteraction = new UserInteraction();
+    private m_objDisp: ObjData3DEntity;
+    private m_axis2: Axis3DEntity;
     initialize(): void {
         console.log("DemoObjModel::initialize()......");
         if (this.m_rscene == null) {
@@ -32,7 +36,7 @@ export class DemoObjModel {
             rparam.setCamPosition(1500.0, 1500.0, 1500.0);
             this.m_rscene = new RendererScene();
             this.m_rscene.initialize(rparam);
-            //this.m_rscene.addEventListener(MouseEvent.MOUSE_DOWN,this,this.mouseDownListener);
+            this.m_rscene.addEventListener(MouseEvent.MOUSE_DOWN,this,this.mouseDownListener);
             this.m_interation.initialize( this.m_rscene );
 
             this.m_texLoader = new ImageTextureLoader(this.m_rscene.textureBlock);
@@ -62,7 +66,52 @@ export class DemoObjModel {
             objDisp.initializeByObjDataUrl(objUrl, [tex1]);
             objDisp.setScaleXYZ(moduleScale, moduleScale, moduleScale);
             //objDisp.setRenderState(RendererState.NONE_CULLFACE_NORMAL_STATE);
+            objDisp.setXYZ(-50, 100, 50);
+            objDisp.update();
             this.m_rscene.addEntity(objDisp);
+            this.m_objDisp = objDisp;
+
+            let axis2: Axis3DEntity = new Axis3DEntity();
+            axis2.initializeCross(5);
+            this.m_axis2 = axis2;
+            // axis2.copyTransformFrom(objDisp);
+            // this.m_rscene.addEntity(axis2);
+
+
+        }
+    }
+    private m_entity: DisplayEntity = null;
+    private mouseDownListener(evt: any): void {
+        if(this.m_entity == null) {
+            let objDisp = this.m_objDisp;
+            // let entity = new DisplayEntity(objDisp.getTransform(), false);
+            let entity = new DisplayEntity(objDisp.getTransform());
+            entity.setMaterial(this.m_axis2.getMaterial());
+            entity.setMesh(this.m_axis2.getMesh());
+            this.m_rscene.addEntity(entity);
+            this.m_entity = entity;
+        }else {
+            this.m_objDisp.setXYZ(50, 100, 50);
+            this.m_objDisp.update();
+            // console.log("A: ", (this.m_objDisp.getTransform() as any).getFS32Data());
+            // console.log("B: ", (this.m_entity.getTransform() as any).getFS32Data());
+            // this.m_entity.getTransform().forceUpdate();
+        }
+    }
+    private mouseDownListener2(evt: any): void {
+        if(this.m_entity == null) {
+            let objDisp = this.m_objDisp;
+            let entity = new DisplayEntity(objDisp.getTransform());
+            entity.setMaterial(this.m_axis2.getMaterial());
+            entity.setMesh(this.m_axis2.getMesh());
+            this.m_rscene.addEntity(entity);
+            this.m_entity = entity;
+        }else {
+            this.m_objDisp.setXYZ(50, 100, 50);
+            this.m_objDisp.update();
+            // console.log("A: ", (this.m_objDisp.getTransform() as any).getFS32Data());
+            // console.log("B: ", (this.m_entity.getTransform() as any).getFS32Data());
+            this.m_entity.getTransform().forceUpdate();
         }
     }
     run(): void {
