5aeae66ae5c4d220602030a2f5654df37db5a283
vilyLei <vily313@126.com>
2022-10-22 21:10:42 +0800

    fix: add the createH5Text function in the CoText module.

--- src/cospace/voxtext/CoText.ts
+++ src/cospace/voxtext/CoText.ts
@@ -1,9 +1,13 @@
-
+import IRendererScene from "../../vox/scene/IRendererScene";
 import { ICoRScene } from "../voxengine/ICoRScene";
+import { H5Text } from "./base/H5Text";
+import { IH5Text } from "./base/IH5Text";
 declare var CoRScene: ICoRScene;
-function test(): void {
-	
+function createH5Text(rscene: IRendererScene, canvasIdName: string, fontSize: number = 10, texWidth: number = 512, texHeight: number = 512): IH5Text {
+	let ht = new H5Text();
+	ht.initialize(rscene, canvasIdName, fontSize, texWidth, texHeight);
+	return ht; 
 }
 export {
-	test
+	createH5Text
 };
--- src/cospace/voxtext/ICoText.ts
+++ src/cospace/voxtext/ICoText.ts
@@ -1,6 +1,15 @@
+import IRendererScene from "../../vox/scene/IRendererScene";
+import { IH5Text } from "./base/IH5Text";
 
-interface ICoUI {
-
-	test(): void;
+interface ICoText {
+	/**
+	 * create a h5 canvas text module
+	 * @param rscene a IRendererScene instance
+	 * @param canvasIdName canvas id name
+	 * @param fontSize the defaule value is 10
+	 * @param texWidth the defaule value is 512
+	 * @param texHeight the defaule value is 512
+	 */
+	createH5Text(rscene: IRendererScene, canvasIdName: string, fontSize?: number, texWidth?: number, texHeight?: number): IH5Text;
 }
-export { ICoUI };
+export { ICoText };
