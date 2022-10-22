8080b4112b11ee110e7e1c84a2eab00ed8bbea1e
vilyLei <vily313@126.com>
2022-10-22 21:16:10 +0800

    fix: add the createStaticTextEntity function in the CoText module.

--- src/cospace/voxtext/CoText.ts
+++ src/cospace/voxtext/CoText.ts
@@ -1,12 +1,20 @@
 import IRendererScene from "../../vox/scene/IRendererScene";
+import IRenderTexture from "../../vox/render/texture/IRenderTexture";
 import { ICoRScene } from "../voxengine/ICoRScene";
 import { H5Text } from "./base/H5Text";
 import { IH5Text } from "./base/IH5Text";
+import { ITextEntity } from "./base/ITextEntity";
+import { TextEntity } from "./base/TextEntity";
 declare var CoRScene: ICoRScene;
 function createH5Text(rscene: IRendererScene, canvasIdName: string, fontSize: number = 10, texWidth: number = 512, texHeight: number = 512): IH5Text {
 	let ht = new H5Text();
 	ht.initialize(rscene, canvasIdName, fontSize, texWidth, texHeight);
 	return ht; 
+}
+function createStaticTextEntity(text: string, h5Text: IH5Text, texList: IRenderTexture[] = null): ITextEntity {
+	let et = new TextEntity();
+	et.initialize(text, h5Text, texList);
+	return et;
 }
 export {
 	createH5Text
--- src/cospace/voxtext/ICoText.ts
+++ src/cospace/voxtext/ICoText.ts
@@ -1,5 +1,7 @@
 import IRendererScene from "../../vox/scene/IRendererScene";
+import IRenderTexture from "../../vox/render/texture/IRenderTexture";
 import { IH5Text } from "./base/IH5Text";
+import { ITextEntity } from "./base/ITextEntity";
 
 interface ICoText {
 	/**
@@ -11,5 +13,12 @@ interface ICoText {
 	 * @param texHeight the defaule value is 512
 	 */
 	createH5Text(rscene: IRendererScene, canvasIdName: string, fontSize?: number, texWidth?: number, texHeight?: number): IH5Text;
+	/**
+	 * create a static text object
+	 * @param text text string
+	 * @param h5Text IH5Text instance
+	 * @param texList IRenderTexture instance array, but the default value is null
+	 */
+	createStaticTextEntity(text: string, h5Text: IH5Text, texList?: IRenderTexture[]): ITextEntity;
 }
 export { ICoText };
