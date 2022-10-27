37147a819998b2ae56dcc4cae62f7357d1943243
vilyLei <vily313@126.com>
2022-10-28 05:40:18 +0800

    fix: add comments info in the GeometryDataUnit class.

--- src/cospace/schedule/base/GeometryDataUnit.ts
+++ src/cospace/schedule/base/GeometryDataUnit.ts
@@ -3,64 +3,72 @@ import { GeometryModelDataType } from "../../modules/base/GeometryModelDataType"
 
 class GeometryDataContainer {
 
-  modelReceiver: (models: GeometryModelDataType[], transforms: Float32Array[], index: number, total: number) => void = null;
-  dataClass: DataClass;
-  dataType: string;
-  dataFormat: DataFormat = DataFormat.CTM;
-  // model: GeometryModelDataType = null;
-  models: GeometryModelDataType[] = null;
-  transforms: Float32Array[] = null;
-  constructor() {
-  }
-  setFormatBUrl(url: string): void {
-    // if(url != "") {
-    //   let k = url.lastIndexOf(".");
-      
-    //   if(k <= 0) {
-    //     throw Error("illegal geometry data url: "+url);
-    //   }
-    //   switch(url.slice(k+1)) {
-    //     case DataFormat.CTM:
-    //       this.dataFormat = DataFormat.CTM;
-    //       break;
-    //     case DataFormat.Draco:
-    //       this.dataFormat = DataFormat.Draco;
-    //       break;
-    //     default:
-    //       throw Error("illegal geometry data url: "+url);
-    //       break;
-    //   }
-    // }
-  }
-  destroy(): void {
-    // this.model = null;
-    this.modelReceiver = null;
-    this.models = null;
-  }
+	dataClass: DataClass;
+	dataType: string;
+	dataFormat: DataFormat = DataFormat.CTM;
+	/**
+	 * 存放所有的model
+	 */
+	models: GeometryModelDataType[] = null;
+	/**
+	 * 存放所有的transform
+	 */
+	transforms: Float32Array[] = null;
+	/**
+	 * 每一次新的model的加入都会调用此函数
+	 */
+	modelReceiver: (models: GeometryModelDataType[], transforms: Float32Array[], index: number, total: number) => void = null;
+	constructor() {
+	}
+	setFormatBUrl(url: string): void {
+		// if(url != "") {
+		//   let k = url.lastIndexOf(".");
+
+		//   if(k <= 0) {
+		//     throw Error("illegal geometry data url: "+url);
+		//   }
+		//   switch(url.slice(k+1)) {
+		//     case DataFormat.CTM:
+		//       this.dataFormat = DataFormat.CTM;
+		//       break;
+		//     case DataFormat.Draco:
+		//       this.dataFormat = DataFormat.Draco;
+		//       break;
+		//     default:
+		//       throw Error("illegal geometry data url: "+url);
+		//       break;
+		//   }
+		// }
+	}
+	destroy(): void {
+		// this.model = null;
+		this.modelReceiver = null;
+		this.models = null;
+	}
 }
 class GeometryDataUnit extends DataUnit {
-  
-  data: GeometryDataContainer = null;
-  constructor() {
-    super();
-    this.dataClass = DataClass.Geometry;
-  }
-  // // 模拟步机制试用
-  // test(): void {
-  //   this.m_timeoutId = setTimeout(this.update.bind(this), 1500);
-  // }
 
-  // private m_timeoutId: any = -1;
-  // /**
-  //  * 定时调度
-  //  */
-  // private update(): void {    
-  //   if (this.m_timeoutId > -1) {
-  //     clearTimeout(this.m_timeoutId);
-  //   }
-  //   console.log("GeometryDataUnit::update();");
-  //   this.toCpuPhase();
-  // }
+	data: GeometryDataContainer = null;
+	constructor() {
+		super();
+		this.dataClass = DataClass.Geometry;
+	}
+	// // 模拟步机制试用
+	// test(): void {
+	//   this.m_timeoutId = setTimeout(this.update.bind(this), 1500);
+	// }
+
+	// private m_timeoutId: any = -1;
+	// /**
+	//  * 定时调度
+	//  */
+	// private update(): void {    
+	//   if (this.m_timeoutId > -1) {
+	//     clearTimeout(this.m_timeoutId);
+	//   }
+	//   console.log("GeometryDataUnit::update();");
+	//   this.toCpuPhase();
+	// }
 }
 
 export { GeometryModelDataType, DataFormat, GeometryDataContainer, DataUnitLock, GeometryDataUnit, DataUnit };
