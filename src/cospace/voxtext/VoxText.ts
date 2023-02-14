c4daeaa093c9402da3db4b644ce5c6b327f5b46b
vilyLei <vily313@126.com>
2023-02-14 13:40:42 +0800

    fix: add some vox modules.

---
+++ src/cospace/voxtext/VoxText.ts
@@ -0,0 +1,56 @@
+import IRendererScene from "../../vox/scene/IRendererScene";
+import IRenderTexture from "../../vox/render/texture/IRenderTexture";
+import { IH5Text } from "./base/IH5Text";
+import { ITextEntity } from "./base/ITextEntity";
+import { ModuleLoader } from "../modules/loaders/ModuleLoader";
+
+import { ICoText } from "./ICoText";
+
+declare var CoText: ICoText;
+
+interface I_CoText {
+}
+class T_CoText {
+
+	private m_init = true;
+	initialize(callback: (urls: string[]) => void = null, url: string = ""): boolean {
+
+		if (this.m_init) {
+			this.m_init = false;
+			if (url == "" || url === undefined) {
+				url = "static/cospace/cotext/CoText.umd.min.js";
+			}
+			new ModuleLoader(1, (): void => {
+				if (callback != null && this.isEnabled()) callback([url]);
+			}).load(url);
+
+			return true;
+		}
+		return false;
+	}
+	isEnabled(): boolean {
+		return typeof CoText !== "undefined";
+	}
+	/**
+	 * create a h5 canvas text module
+	 * @param rscene a IRendererScene instance
+	 * @param canvasIdName canvas id name
+	 * @param fontSize the defaule value is 10
+	 * @param texWidth the defaule value is 512
+	 * @param texHeight the defaule value is 512
+	 */
+	createH5Text(rscene: IRendererScene, canvasIdName: string, fontSize?: number, texWidth?: number, texHeight?: number): IH5Text {
+		return CoText.createH5Text(rscene, canvasIdName, fontSize, texWidth, texHeight);
+	}
+	/**
+	 * create a static text object
+	 * @param text text string
+	 * @param h5Text IH5Text instance
+	 * @param texList IRenderTexture instance array, but the default value is null
+	 */
+	createStaticTextEntity(text: string, h5Text: IH5Text, texList?: IRenderTexture[]): ITextEntity {
+		return CoText.createStaticTextEntity(text, h5Text, texList);
+	}
+}
+let VoxText = new T_CoText();
+export { VoxText };
---
+++ src/cospace/voxtexture/VoxTexture.ts
@@ -0,0 +1,56 @@
+import IRendererScene from "../../vox/scene/IRendererScene";
+import IColor4 from "../../vox/material/IColor4";
+import IImageTexAtlas from "./atlas/IImageTexAtlas";
+import ICanvasTexAtlas from "./atlas/ICanvasTexAtlas";
+import { ModuleLoader } from "../modules/loaders/ModuleLoader";
+
+import { CoImageTexAtlas, ICoTexture } from "./ICoTexture";
+
+declare var CoTexture: ICoTexture;
+
+interface I_CoTexture {
+}
+class T_CoTexture {
+
+	private m_init = true;
+	initialize(callback: (urls: string[]) => void = null, url: string = ""): boolean {
+
+		if (this.m_init) {
+			this.m_init = false;
+			if (url == "" || url === undefined) {
+				url = "static/cospace/cotexture/CoTexture.umd.min.js";
+			}
+			new ModuleLoader(1, (): void => {
+				if (callback != null && this.isEnabled()) callback([url]);
+			}).load(url);
+
+			return true;
+		}
+		return false;
+	}
+	isEnabled(): boolean {
+		return typeof CoTexture !== "undefined";
+	}
+
+	get ImageTexAtlas(): CoImageTexAtlas {
+		return CoTexture.ImageTexAtlas;
+	}
+	createCharsImage(chars: string, fontSize: number, fontColor?: IColor4, bgColor?: IColor4): HTMLCanvasElement | HTMLImageElement {
+		return CoTexture.createCharsImage(chars, fontSize, fontColor, bgColor);
+	}
+	createCanvasTexAtlas(): ICanvasTexAtlas {
+		return CoTexture.createCanvasTexAtlas();
+	}
+	createImageTexAtlas(
+		rscene: IRendererScene,
+		canvasWidth: number,
+		canvasHeight: number,
+		fillColor: IColor4,
+		transparent?: boolean,
+		nearestFilter?: boolean
+	): IImageTexAtlas {
+		return CoTexture.createImageTexAtlas(rscene, canvasWidth, canvasHeight, fillColor, transparent, nearestFilter);
+	}
+}
+let VoxTexture = new T_CoTexture();
+export { VoxTexture };
