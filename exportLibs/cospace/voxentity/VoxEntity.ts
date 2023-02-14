37b5b71934305da209711abf15d854b1738f2d26
vilyLei <vily313@126.com>
2023-02-14 19:08:49 +0800

    fix: add a engine lib file.

---
+++ exportLibs/cospace/voxentity/VoxEntity.ts
@@ -0,0 +1,102 @@
+import IVector3D from "../../vox/math/IVector3D";
+import IRenderMaterial from "../../vox/render/IRenderMaterial";
+import ITransformEntity from "../../vox/entity/ITransformEntity";
+import IDisplayEntityContainer from "../../vox/entity/IDisplayEntityContainer";
+import IMouseEventEntity from "../../vox/entity/IMouseEventEntity";
+import IBoundsEntity from "../../vox/entity/IBoundsEntity";
+import IDataMesh from "../../vox/mesh/IDataMesh";
+import { ModuleLoader } from "../modules/loaders/ModuleLoader";
+
+import { CoGeomDataType, CoTextureDataUnit, CoGeomDataUnit } from "../app/CoSpaceAppData";
+
+import { ICoEntity } from "./ICoEntity";
+declare var CoEntity: ICoEntity;
+
+import { ICoRScene } from "../voxengine/ICoRScene";
+declare var CoRScene: ICoRScene;
+
+interface I_CoEntity {
+}
+
+class T_CoEntity {
+
+	private m_init = true;
+	initialize(callback: (urls: string[]) => void = null, url: string = ""): boolean {
+
+		if (this.m_init) {
+			this.m_init = false;
+			if (url == "" || url === undefined) {
+				url = "static/cospace/coentity/CoEntity.umd.min.js";
+			}
+			new ModuleLoader(1, (): void => {
+				if (callback != null && this.isEnabled()) callback([url]);
+			}).load(url);
+
+			return true;
+		}
+		return false;
+	}
+	isEnabled(): boolean {
+		return typeof CoEntity !== "undefined" && typeof CoRScene !== "undefined";
+	}
+	/**
+	 * @param model geometry model
+	 * @param material IRenderMaterial instance, the default is null.
+	 * @param vbWhole vtx buffer is whole data, or not, the default is false.
+	 */
+	createDataMeshFromModel(model: CoGeomDataType, material?: IRenderMaterial, vbWhole?: boolean): IDataMesh {
+		return CoEntity.createDataMeshFromModel(model, material, vbWhole);
+	}
+	/**
+	 * @param model geometry model
+	 * @param pmaterial IRenderMaterial instance, the default is null.
+	 * @param texEnabled texture enabled in the material, the default is true.
+	 * @param vbWhole vtx buffer is whole data or not, the default is false.
+	 */
+	createDisplayEntityFromModel(model: CoGeomDataType, pmaterial?: IRenderMaterial, texEnabled?: boolean, vbWhole?: boolean): ITransformEntity {
+		return CoEntity.createDisplayEntityFromModel(model, pmaterial, texEnabled, vbWhole);
+	}
+
+	createFreeAxis3DEntity(minV: IVector3D, maxV: IVector3D): ITransformEntity {
+		return CoEntity.createFreeAxis3DEntity(minV, maxV);
+	}
+	/**
+	 * @param size th default value is 100.0
+	 */
+	createAxis3DEntity(size?: number): ITransformEntity {
+		return CoEntity.createAxis3DEntity(size);
+	}
+
+	/**
+	 * @param model IDataMesh instance
+	 * @param material IRenderMaterial instance.
+	 * @param texEnabled use texture yes or no.
+	 * @param vbWhole vtx buffer is whole data, or not, the default is false.
+	 */
+	createDisplayEntityWithDataMesh(mesh: IDataMesh, material: IRenderMaterial, texEnabled?: boolean, vbWhole?: boolean): ITransformEntity {
+		return CoEntity.createDisplayEntityWithDataMesh(mesh, material, texEnabled, vbWhole);
+	}
+	createDisplayEntity(): ITransformEntity {
+		return CoEntity.createDisplayEntity();
+	}
+	createMouseEventEntity(): IMouseEventEntity {
+		return CoEntity.createMouseEventEntity();
+	}
+	createBoundsEntity(): IBoundsEntity {
+		return CoEntity.createBoundsEntity();
+	}
+
+	/**
+	 * create a cross axis randerable entity
+	 * @param size the default value is 100
+	 */
+	createCrossAxis3DEntity(size: number): ITransformEntity {
+		return CoEntity.createCrossAxis3DEntity( size );
+	}
+
+	createDisplayEntityContainer(): IDisplayEntityContainer {
+		return CoEntity.createDisplayEntityContainer();
+	}
+}
+const VoxEntity = new T_CoEntity();
+export { VoxEntity };
