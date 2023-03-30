ebfd3523c4c09a70500341865dc7a5cde27103cc
vilyLei <vily313@126.com>
2023-03-30 23:54:42 +0800

    fix: remove some redundant codes in the container rop block.

--- src/vox/render/ContainerRPOBlock.ts
+++ src/vox/render/ContainerRPOBlock.ts
@@ -5,11 +5,9 @@
 /*                                                                         */
 /***************************************************************************/
 
-import RPOUnit from "../../vox/render/RPOUnit";
 import RPONode from "../../vox/render/RPONode";
 import RenderProxy from "../../vox/render/RenderProxy";
 import RenderShader from "../../vox/render/RenderShader";
-import IRODisplaySorter from "../../vox/render/IRODisplaySorter";
 import RPOBlock from "./RPOBlock";
 import PassProcess from "./pass/PassProcess";
 import IRenderEntityContainer from "./IRenderEntityContainer";
@@ -69,91 +67,12 @@ export default class ContainerRPOBlock extends RPOBlock {
 		return this.m_container == null;
 	}
 	private m_passProc = new PassProcess();
-	// private m_shdUpdate = false;
-	/*
-	private runContainer(rc: RenderProxy, container: IRenderEntityContainer): void {
-		const els = container.getEntities();
-		const elen = els.length;
-		if (elen > 0) {
-			const proc = this.m_passProc;
-			proc.shader = this.m_shader;
-			// 可能需要排序
-			for (let i = 0; i < elen; ++i) {
-				const et = els[i];
-				if (et.isPolyhedral() && et.isVisible() && et.isInRenderer()) {
-					const unit = et.getDisplay().__$$runit as RPOUnit;
-					if (unit && unit.rendering) {
-						this.m_shader.bindToGpu(unit.shdUid);
-						unit.updateVtx();
-						if (unit.drawing) {
-							if (unit.rgraph && unit.rgraph.isEnabled()) {
-								proc.units = [unit];
-								proc.rc = rc;
-								proc.vtxFlag = true;
-								proc.texFlag = true;
-								unit.rgraph.run(proc);
-								this.m_shdUpdate = true;
-							} else {
-								if (this.m_shdUpdate) {
-									unit.applyShader(true);
-									this.m_shdUpdate = false;
-								}
-								unit.run(rc);
-								unit.draw(rc);
-							}
-						}
-					}
-				}
-			}
-		}
-		const cls = container.getContainers();
-		const clen = cls.length;
-		if (clen > 0) {
-			for (let i = 0; i < clen; ++i) {
-				this.runContainer(rc, cls[i]);
-			}
-		}
-	}
 
-	private runContainerLock(rc: RenderProxy, container: IRenderEntityContainer): void {
-		const els = container.getEntities();
-		const elen = els.length;
-		if (elen > 0) {
-			const proc = this.m_passProc;
-			proc.shader = this.m_shader;
-			// 可能需要排序
-			for (let i = 0; i < elen; ++i) {
-				const et = els[i];
-				if (et.isPolyhedral() && et.isVisible() && et.isInRenderer()) {
-					const unit = et.getDisplay().__$$runit as RPOUnit;
-					if (unit && unit.rendering) {
-						this.m_shader.bindToGpu(unit.shdUid);
-						unit.updateVtx();
-						if (unit.drawing) {
-							unit.vro.run();
-							unit.runLockMaterial2(null);
-							unit.draw(rc);
-						}
-					}
-				}
-			}
-		}
-		const cls = container.getContainers();
-		const clen = cls.length;
-		if (clen > 0) {
-			for (let i = 0; i < clen; ++i) {
-				this.runContainerLock(rc, cls[i]);
-			}
-		}
-	}
-	//*/
 	run(rc: RenderProxy): void {
 		// this.m_shader.resetUniform();
 		// console.log("ContainerRPOBlock::run() ...");
 		const c = this.m_container;
 		if(c && c.isVisible()) {
-			// this.m_shdUpdate = false;
-			// this.runContainer(rc, c);
 			this.resetCR();
 			this.m_cr.runContainer(rc, c);
 		}
@@ -162,7 +81,6 @@ export default class ContainerRPOBlock extends RPOBlock {
 		// this.m_shader.resetUniform();
 		const c = this.m_container;
 		if(c && c.isVisible()) {
-			// this.runContainerLock(rc, c);
 			this.resetCR();
 			this.m_cr.runContainerLock(rc, c);
 		}
