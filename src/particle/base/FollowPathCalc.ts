4c28d4b191e00a88938d7b109850c69581193c75
vilyLei <vily313@126.com>
2023-04-03 22:24:26 +0800

    fix: add a following path calc codes.

---
+++ src/particle/base/FollowPathCalc.ts
@@ -0,0 +1,15 @@
+/***************************************************************************/
+/*                                                                         */
+/*  Copyright 2018-2022 by                                                 */
+/*  Vily(vily313@126.com)                                                  */
+/*                                                                         */
+/***************************************************************************/
+
+import Vector3D from "../../vox/math/Vector3D";
+
+class FollowPathCalc {
+
+	constructor() { }
+
+}
+export { FollowPathCalc }
