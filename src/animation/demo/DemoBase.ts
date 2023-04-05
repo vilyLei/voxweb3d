778a51818f680960e6a9bf2e82478ae056dad291
vilyLei <vily313@126.com>
2023-04-06 05:17:46 +0800

    fix: add a base demo in the animation module.

---
+++ src/animation/demo/DemoBase.ts
@@ -0,0 +1,85 @@
+import RendererDevice from "../../vox/render/RendererDevice";
+import RendererParam from "../../vox/scene/RendererParam";
+
+import DisplayEntity from "../../vox/entity/DisplayEntity";
+import RendererScene from "../../vox/scene/RendererScene";
+import DivLog from "../../vox/utils/DivLog";
+import Default3DMaterial from "../../vox/material/mcase/Default3DMaterial";
+import Box3DEntity from "../../vox/entity/Box3DEntity";
+import RenderStatusDisplay from "../../vox/scene/RenderStatusDisplay";
+import { MouseInteraction } from "../../vox/ui/MouseInteraction";
+import DefaultPassGraph from "../../vox/render/pass/DefaultPassGraph";
+import Sphere3DEntity from "../../vox/entity/Sphere3DEntity";
+import IRenderTexture from "../../vox/render/texture/IRenderTexture";
+import TextureResLoader from "../../vox/assets/TextureResLoader";
+import TextureConst from "../../vox/texture/TextureConst";
+
+export class DemoBase {
+	private m_init = true;
+	private m_texLoader: TextureResLoader = null;
+	constructor() {}
+
+	private getAssetTexByUrl(pns: string): IRenderTexture {
+		return this.getTexByUrl("static/assets/" + pns);
+	}
+	private getTexByUrl(url: string, preAlpha: boolean = false, wrapRepeat: boolean = true, mipmapEnabled = true): IRenderTexture {
+		let hostUrl = window.location.href;
+		if (hostUrl.indexOf(".artvily.") > 0) {
+			hostUrl = "http://www.artvily.com:9090/";
+			url = hostUrl + url;
+		}
+		return this.m_texLoader.getTexByUrl(url, preAlpha, wrapRepeat, mipmapEnabled);
+	}
+
+	initialize(): void {
+		console.log("DemoBase::initialize()......");
+		if (this.m_init) {
+			this.m_init = false;
+
+			document.oncontextmenu = function(e) {
+				// e.preventDefault();
+			};
+
+			RendererDevice.SHADERCODE_TRACE_ENABLED = false;
+			RendererDevice.VERT_SHADER_PRECISION_GLOBAL_HIGHP_ENABLED = true;
+
+			let rparam = new RendererParam();
+			rparam.setCamProject(45, 0.1, 6000.0);
+			rparam.setCamPosition(1100.0, 1100.0, 1100.0);
+			// 保持 html body color 和 renderer clear color 同步，以便正确表现alpha混合
+			// rparam.syncBgColor = false;
+
+			let rscene = new RendererScene();
+			rscene.initialize(rparam).setAutoRunning(true);
+			rscene.setClearRGBAColor4f(0.0, 0.0, 0.0, 1.0);
+
+			this.m_texLoader = new TextureResLoader(rscene);
+
+			new MouseInteraction().initialize(rscene, 0, true).setAutoRunning(true);
+			new RenderStatusDisplay(rscene, true);
+
+			this.initScene(rscene);
+		}
+	}
+	private testDataMesh(rscene: RendererScene): void {
+
+		let sph = new Sphere3DEntity();
+		sph.initialize(160, 20, 20, [this.getTexByUrl("static/assets/default.jpg", true)]);
+		sph.setXYZ(100,0, 200);
+		rscene.addEntity(sph, 1);
+
+	}
+
+	private initScene(rscene: RendererScene): void {
+		this.initEntities(rscene);
+		this.testDataMesh(rscene);
+	}
+
+	private initEntities(rscene: RendererScene): void {
+		let box0 = new Box3DEntity();
+		box0.normalEnabled = true;
+		box0.initializeCube(100, [this.getTexByUrl("static/assets/box.jpg")]);
+		rscene.addEntity(box0);
+	}
+}
+export default DemoBase;
