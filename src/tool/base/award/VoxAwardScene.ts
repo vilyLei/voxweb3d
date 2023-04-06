43f180cba2151f3ac112e28569082127b980df47
vilyLei <vily313@126.com>
2023-04-06 21:56:23 +0800

    fix: add a vox icon res img.

--- src/tool/base/award/VoxAwardScene.ts
+++ src/tool/base/award/VoxAwardScene.ts
@@ -19,13 +19,13 @@ class VoxAwardScene {
 	private initScene(): void {
 
 		let vparam = this.m_vasParam;
-		let tex = vparam.getTexByUrl("static/assets/default.jpg");
+		let tex = vparam.getTexByUrl("static/assets/vox.png");
 		tex.flipY = true;
 		let pw = 130;
 		let ph = 130;
 		let pl0 = vparam.createXOYPlane(0,0, pw, ph, tex);
 		let st = this.m_uisc.getStage3D();
-		pl0.setXYZ(st.stageWidth - pw - 20, st.stageHeight - ph - 60, 0);
+		pl0.setXYZ(st.stageWidth - pw - 20, st.stageHeight - ph - 20, 0);
 		this.m_uisc.addEntity(pl0, vparam.pid);
 	}
 }
