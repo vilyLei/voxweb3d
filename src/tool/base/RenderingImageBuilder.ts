9b838d16b2e0ca98b8932c1f5fd0d9be76716c1d
vilyLei <vily313@126.com>
2023-04-11 06:13:29 +0800

    fix: adjust the rendering image builder class codes.

--- src/tool/base/RenderingImageBuilder.ts
+++ src/tool/base/RenderingImageBuilder.ts
@@ -1,6 +1,7 @@
 import TextureResLoader from "../../vox/assets/TextureResLoader";
 import RendererDevice from "../../vox/render/RendererDevice";
 import IRenderTexture from "../../vox/render/texture/IRenderTexture";
+import IRendererParam from "../../vox/scene/IRendererParam";
 import IRendererScene from "../../vox/scene/IRendererScene";
 import RendererSceneGraph from "../../vox/scene/RendererSceneGraph";
 
@@ -9,7 +10,7 @@ export class RenderingImageBuilder {
 
 	private m_graph = new RendererSceneGraph();
 	texLoader: TextureResLoader = null;
-	private m_rscene: IRendererScene = null;
+	rscene: IRendererScene = null;
 	
 	constructor() {}
 	getAssetTexByUrl(pns: string): IRenderTexture {
@@ -24,32 +25,30 @@ export class RenderingImageBuilder {
 		return this.texLoader.getTexByUrl(url, preAlpha, wrapRepeat, mipmapEnabled);
 	}
 
-	initialize(): void {
+	initialize(rparam: IRendererParam): void {
 		console.log("RenderingImageBuilder::initialize()......");
 		if (this.m_init) {
 			this.m_init = false;
 
-			document.oncontextmenu = function(e) {
-				e.preventDefault();
-			};
-			RendererDevice.SHADERCODE_TRACE_ENABLED = false;
-			RendererDevice.VERT_SHADER_PRECISION_GLOBAL_HIGHP_ENABLED = true;
+			// document.oncontextmenu = function(e) {
+			// 	e.preventDefault();
+			// };
+			// RendererDevice.SHADERCODE_TRACE_ENABLED = false;
+			// RendererDevice.VERT_SHADER_PRECISION_GLOBAL_HIGHP_ENABLED = true;
 
 			// let rparam = new RendererParam(this.createDiv(0,0, 512, 512));
-			let rparam = this.m_graph.createRendererParam();
-			rparam.setCamProject(45, 0.1, 6000.0);
-			rparam.setCamPosition(1100.0, 1100.0, 1100.0);
+			// let rparam = this.m_graph.createRendererParam();
+			// rparam.setCamProject(45, 0.1, 6000.0);
+			// rparam.setCamPosition(1100.0, 1100.0, 1100.0);
 			// rparam.setAttriAlpha(true);
-			rparam.setAttriAntialias(true);
-			rparam.autoSyncRenderBufferAndWindowSize = false;
+			// // rparam.setAttriAntialias(true);
+			// rparam.offscreenRenderEnabled = true;
+			// rparam.autoAttachingHtmlDoc = !rparam.offscreenRenderEnabled;
 			// rparam.autoSyncRenderBufferAndWindowSize = false;
-			// 保持 html body color 和 renderer clear color 同步，以便正确表现alpha混合
-			// rparam.syncBgColor = false;
-
+			// rparam.syncBgColor = true;
 			let rscene = this.m_graph.createScene(rparam);
-			// rscene.initialize(rparam).setAutoRunning(true);
-			rscene.setClearRGBAColor4f(0.2, 0.3, 0.2, 0.0);
-			this.m_rscene = rscene;
+			rscene.setClearRGBAColor4f(0.0, 0.0, 0.0, 0.0);
+			rscene = rscene;
 
 		}
 	}
