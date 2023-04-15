a1d2bd0c384a7836db6836fe5f6f2ce748c90a29
vilyLei <vily313@126.com>
2023-04-15 19:05:01 +0800

    fix: update the default material shader codes.

--- src/vox/material/mcase/Default3DMaterial.ts
+++ src/vox/material/mcase/Default3DMaterial.ts
@@ -18,6 +18,8 @@ class Default3DShaderCodeBuffer extends ShaderCodeBuffer {
     vtxMatrixTransform = true;
     tns = "";
 	fragMainTailCode = "\n";
+    alignScreen = false;
+    fixAlignScreen = false;
     constructor() {
         super();
     }
@@ -28,22 +30,32 @@ class Default3DShaderCodeBuffer extends ShaderCodeBuffer {
         if (this.vertColorEnabled) this.m_uniqueName += "_vtxColor";
         if (this.premultiplyAlpha) this.m_uniqueName += "_preMulAlpha";
         this.adaptationShaderVersion = false;
+        if(this.fixAlignScreen) {
+            this.m_uniqueName += "_fixAlScr";
+        }else if(this.alignScreen){
+            this.m_uniqueName += "_alScr";
+        }
     }
 
     buildShader(): void {
 
         let coder = this.m_coder;
         coder.addVertLayout("vec3", "a_vs");
-        coder.addFragUniform("vec4", "u_color");
+        coder.addFragUniform("vec4", "u_fragParams", 3);
 
+        coder.useVertSpaceMats(false, false, false);
+        if(this.fixAlignScreen) {
+            this.vtxMatrixTransform = false;
+            coder.addDefine("VOX_FIX_ALIGN_SCREEN");
+        }else if(this.alignScreen){
+            this.vtxMatrixTransform = false;
+            coder.useVertSpaceMats(true, false, false);
+            coder.addDefine("VOX_ALIGN_SCREEN");
+        }
         if (this.vtxMatrixTransform) {
             coder.addDefine("VOX_VTX_MAT_TRANSFORM");
             coder.useVertSpaceMats(true, true, true);
         }
-        else {
-            coder.useVertSpaceMats(false, false, false);
-        }
-
         if (this.m_texEnabled) {
             this.m_uniform.addDiffuseMap();
             coder.addVertLayout("vec2", "a_uvs");
@@ -57,6 +69,7 @@ class Default3DShaderCodeBuffer extends ShaderCodeBuffer {
         /*
         coder.addVertHeadCode(
 `
+//  FragColor0 *= VOX_Texture2D(VOX_DIFFUSE_MAP, vec2(v_uv[0],v_uv[1]));
 float calcValue(float px) {
 
     if(px > 1.0) {
@@ -84,23 +97,30 @@ vec2 getUV(vec2 uv) {
 
         coder.addFragOutput("vec4", "FragColor0");
 
-		let fragMainTailCode = "\n";
+		// let fragMainTailCode = "\n";
         coder.addFragMainCode(
             `
     FragColor0 = vec4(1.0);
     #ifdef VOX_USE_2D_MAP
-        //  FragColor0 *= VOX_Texture2D(VOX_DIFFUSE_MAP, vec2(v_uv[0],v_uv[1]));
-        FragColor0 *= VOX_Texture2D(VOX_DIFFUSE_MAP, v_uv.xy);
+        #ifdef VOX_Texture2DLod
+            vec4 color4 = VOX_Texture2DLod(VOX_DIFFUSE_MAP, v_uv, u_param[2].w);
+        #else
+            FragColor0 *= VOX_Texture2D(VOX_DIFFUSE_MAP, v_uv.xy);
+        #endif
     #endif
     #ifdef VOX_USE_VTX_COLOR
         FragColor0.xyz *= v_cv.xyz;
     #endif
+    vec4 param = u_fragParams[0];
+    vec4 offsetParam = u_fragParams[1];
     #ifdef VOX_PREMULTIPLY_ALPHA
-        FragColor0.rgb *= u_color.xyz;
-        FragColor0.a *= u_color.w;
-        FragColor0.rgb *= u_color.aaa;
+        FragColor0 *= param;
+        FragColor0.xyz += offsetParam.xyz;
+        param.w += offsetParam.w;
+        FragColor0.xyz *= param.www;
     #else
-        FragColor0 *= u_color;
+        FragColor0 *= param;
+        FragColor0 += offsetParam;
     #endif
     #ifdef VOX_USE_NORMAL
         float nDotL = max(dot(v_worldNormal.xyz, direc), 0.0);
@@ -122,7 +142,11 @@ vec2 getUV(vec2 uv) {
             v_worldNormal = normalize( a_nvs.xyz * inverse(mat3(u_objMat)) );
         #endif
     #else
-        gl_Position = localPosition;
+        #ifdef VOX_ALIGN_SCREEN
+            gl_Position = localPosition;
+        #else
+            gl_Position = localPosition;
+        #endif
         #ifdef VOX_USE_NORMAL
             v_worldNormal = normalize( a_nvs.xyz );
         #endif
@@ -145,7 +169,11 @@ vec2 getUV(vec2 uv) {
 export default class Default3DMaterial extends MaterialBase implements IDefault3DMaterial {
 
     private static s_shdCodeBuffer: Default3DShaderCodeBuffer = null;
-    private m_data = new Float32Array([1.0, 1.0, 1.0, 1.0]);
+    private m_data = new Float32Array([
+        1.0, 1.0, 1.0, 1.0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+    ]);
     private m_uvTrans = new Float32Array([0.0, 0.0, 1.0, 1.0]);
     name = "";
 	fragMainTailCode = "";
@@ -154,6 +182,8 @@ export default class Default3DMaterial extends MaterialBase implements IDefault3
     normalEnabled = false;
     shadowReceiveEnabled = false;
     vtxMatrixTransform = true;
+    alignScreen = false;
+    fixAlignScreen = false;
     constructor() {
         super();
         if (Default3DMaterial.s_shdCodeBuffer == null) {
@@ -216,9 +246,24 @@ export default class Default3DMaterial extends MaterialBase implements IDefault3
     getColor(color: Color4): void {
         color.fromArray4(this.m_data);
     }
+    
+    setOffsetRGB3f(pr: number, pg: number, pb: number): void {
+        this.m_data[4] = pr;
+        this.m_data[5] = pg;
+        this.m_data[6] = pb;
+    }
+    setOffsetRGBA4f(pr: number, pg: number, pb: number, pa: number): void {
+        this.m_data[4] = pr;
+        this.m_data[5] = pg;
+        this.m_data[6] = pb;
+        this.m_data[7] = pa;
+    }
+    setTextureLodLevel(lodLv: number): void {
+        this.m_data[11] = lodLv;
+    }
     createSelfUniformData(): ShaderUniformData {
         let oum = new ShaderUniformData();
-        oum.uniformNameList = ["u_color", "u_uvTrans"];
+        oum.uniformNameList = ["u_fragParams", "u_uvTrans"];
         oum.dataList = [this.m_data, this.m_uvTrans];
         return oum;
     }
